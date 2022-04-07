b5468948750fc55403dc132a03f8281ea4b73b65
黄孟柱 <hmzzrcs@163.com>
2022-04-07 12:09:34 +0800

    提交

--- modules/routers/routers.go
+++ modules/routers/routers.go
@@ -11,23 +11,23 @@ type Routers struct {
 
 func (p *Routers) Lookup(r *http.Request) (view string, data interface{}, has bool) {
 	name, has := p.views.Lookup(r)
-	if has{
-		return name,nil,true
+	if has {
+		return name, nil, true
 	}
-	return "",nil,false
+	return "", nil, false
 }
 
 func (p *Routers) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-
 }
 
 func NewRouters() *Routers {
 	views := map[string]string{
-		"list":"router_list",
-		"create":"router_create",
-		"edit":"router_edit",
+		"list":   "router_list",
+		"create": "router_create",
+		"edit":   "router_edit",
 	}
+
 	return &Routers{
-		views:	apinto_dashboard.NewViewModuleEmpty("/routers/", views, "list"),
+		views: apinto_dashboard.NewViewModuleEmpty("/routers/", views, "list"),
 	}
 }
