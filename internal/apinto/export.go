09f479897c82cdd96f77a977ab2c3223b95c6cc0
黄孟柱 <hmzzrcs@163.com>
2022-04-07 11:55:33 +0800

    提交

---
+++ internal/apinto/export.go
@@ -0,0 +1,16 @@
+package apinto
+
+var (
+	nodes = "http://127.0.0.1:8080"
+)
+
+func List(professions string) {
+
+}
+func GET(id string) {
+
+}
+
+func PUT(profession, name string, data interface{}) {
+
+}
