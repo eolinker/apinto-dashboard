886c9c232033ae5466888bb02900288bc4cf34bc
黄孟柱 <hmzzrcs@163.com>
2022-04-07 12:09:27 +0800

    提交

--- modules/professions/professions.go
+++ modules/professions/professions.go
@@ -3,34 +3,35 @@ package professions
 import (
 	"fmt"
 	apinto_dashboard "github.com/eolinker/apinto-dashboard"
+	"github.com/eolinker/apinto-dashboard/internal/apinto"
 	"net/http"
 )
 
 type Profession struct {
-	views *apinto_dashboard.ModuleViewFinder
+	views          *apinto_dashboard.ModuleViewFinder
 	professionName string
 }
 
 func (p *Profession) Lookup(r *http.Request) (view string, data interface{}, has bool) {
 	name, has := p.views.Lookup(r)
-	if has{
-		return name,nil,true
+	if has {
+		return name, nil, true
 	}
-	return "",nil,false
+	return "", nil, false
 }
 
 func (p *Profession) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-
+	apinto.Handler().ServeHTTP(w, req)
 }
 
-func NewProfession(name string,profession string) *Profession {
+func NewProfession(name string, profession string) *Profession {
 	views := map[string]string{
-		"list":"profession_list",
-		"create":"profession_create",
-		"edit":"profession_edit",
+		"list":   "profession_list",
+		"create": "profession_create",
+		"edit":   "profession_edit",
 	}
 	return &Profession{
-		professionName:profession,
-		views:	apinto_dashboard.NewViewModuleEmpty(fmt.Sprint("/", name, "/"), views, "list"),
+		professionName: profession,
+		views:          apinto_dashboard.NewViewModuleEmpty(fmt.Sprint("/", name, "/"), views, "list"),
 	}
 }
