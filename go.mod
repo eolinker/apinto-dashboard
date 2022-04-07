6412a934ac53cc349ed31e29d1ac1476bedbb23c
黄孟柱 <hmzzrcs@163.com>
2022-04-07 12:09:34 +0800

    提交

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/eolinker/apinto-dashboard
 
 go 1.17
 
-require gopkg.in/yaml.v2 v2.4.0
+require (
+	github.com/julienschmidt/httprouter v1.3.0
+	gopkg.in/yaml.v2 v2.4.0
+)
--- internal/apinto/openapi.go
+++ internal/apinto/openapi.go
@@ -1,2 +1,29 @@
 package apinto
 
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+var (
+	professionRouterClientHandler *ProfessionRouter = NewProfessionRouter()
+)
+
+func Handler() http.Handler {
+	return professionRouterClientHandler
+}
+
+type ProfessionRouter struct {
+	*httprouter.Router
+}
+
+func NewProfessionRouter() *ProfessionRouter {
+	r := httprouter.New()
+	r.GET("/api/:prfession/:name", func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+
+	})
+	r.GET("/api/:prfession", func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+
+	})
+	return &ProfessionRouter{}
+}
