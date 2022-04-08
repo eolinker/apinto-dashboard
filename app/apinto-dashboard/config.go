b38a2ef27f55a6d7e3c963e424c2c293b8c3d86f
黄孟柱 <hmzzrcs@163.com>
2022-04-08 10:58:22 +0800

    list data

--- app/apinto-dashboard/config.go
+++ app/apinto-dashboard/config.go
@@ -8,50 +8,65 @@ import (
 	"io/ioutil"
 	"strings"
 )
+
 type ProfessionConfigItem struct {
-	Name string `yaml:"name"`
-	I18nNames map[string]string `yaml:"i18n_name"`
-	Profession string `yaml:"professions"`
+	Name       string            `yaml:"name"`
+	I18nNames  map[string]string `yaml:"i18n_name"`
+	Titles     map[string]string `yaml:"titles"`
+	Fields 	   []string `yaml:"fields"`
+	Profession string            `yaml:"professions"`
 }
 
 type Config struct {
-	Zone string `yaml:"zone"`
-	Default string `yaml:"default"`
+	Zone        string                  `yaml:"zone"`
+	Default     string                  `yaml:"default"`
 	Professions []*ProfessionConfigItem `yaml:"professions"`
 }
 
-func ReadConfig(file string) (*Config,error) {
+func ReadConfig(file string) (*Config, error) {
 
 	data, err := ioutil.ReadFile(file)
 	if err != nil {
 		return nil, err
 	}
 
-	c:=new(Config)
+	c := new(Config)
 	err = yaml.Unmarshal(data, c)
 	if err != nil {
 		return nil, err
 	}
-	if c.Zone == ""{
+	if c.Zone == "" {
 		c.Zone = string(apinto.ZhCn)
 	}
-	if c.Default == "" && len(c.Professions)>0{
+	if c.Default == "" && len(c.Professions) > 0 {
 		c.Default = c.Professions[0].Name
 	}
-	return c,nil
+	return setDefault(c), nil
 }
+func setDefault(c*Config) *Config  {
+	if c.Zone == "" {
+		c.Zone = string(apinto.ZhCn)
+	}
+	if c.Default == "" && len(c.Professions) > 0 {
+		c.Default = c.Professions[0].Name
+	}
 
-func toModule(c *Config)[]*apinto.Module  {
-	r:=make([]*apinto.Module,0,len(c.Professions))
-	for _,cm:=range c.Professions{
-		m:=&apinto.Module{
-			Path:     fmt.Sprintf("/%s/list",cm.Name),
-			Handler:  professions.NewProfession(cm.Name,cm.Profession),
+	for _,p:=range c.Professions{
+		if p.Titles[]
+	}
+
+}
+func toModule(c *Config) []*apinto.Module {
+	r := make([]*apinto.Module, 0, len(c.Professions))
+	for _, cm := range c.Professions {
+		m := &apinto.Module{
+			Path:     fmt.Sprintf("/%s/list", cm.Name),
+			Handler:  professions.NewProfession(cm.Name, cm.Profession, nil, nil),
 			Name:     cm.Name,
 			I18nName: make(map[apinto.ZoneName]string),
 		}
-		for k,v:=range cm.I18nNames{
-			m.I18nName[apinto.ZoneName(strings.ToLower(k))]=v
+		for k, v := range cm.I18nNames {
+			m.I18nName[apinto.ZoneName(strings.ToLower(k))] = v
 		}
 		r = append(r, m)
 	}
