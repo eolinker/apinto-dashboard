069b89f3cb89942b772a393c8fc06a602f9d354b
黄孟柱 <hmzzrcs@163.com>
2022-04-08 15:03:12 +0800

    前端

--- app/apinto-dashboard/config.go
+++ app/apinto-dashboard/config.go
@@ -10,11 +10,11 @@ import (
 )
 
 type ProfessionConfigItem struct {
-	Name       string            `yaml:"name"`
-	I18nNames  map[string]string `yaml:"i18n_name"`
-	Titles     map[string]string `yaml:"titles"`
-	Fields 	   []string `yaml:"fields"`
-	Profession string            `yaml:"professions"`
+	Name       string              `yaml:"name"`
+	I18nNames  map[string]string   `yaml:"i18n_name"`
+	Titles     map[string][]string `yaml:"titles"`
+	Fields     []string            `yaml:"fields"`
+	Profession string              `yaml:"professions"`
 }
 
 type Config struct {
@@ -41,27 +41,45 @@ func ReadConfig(file string) (*Config, error) {
 	if c.Default == "" && len(c.Professions) > 0 {
 		c.Default = c.Professions[0].Name
 	}
-	return setDefault(c), nil
+	return c, nil
 }
-func setDefault(c*Config) *Config  {
-	if c.Zone == "" {
-		c.Zone = string(apinto.ZhCn)
-	}
-	if c.Default == "" && len(c.Professions) > 0 {
-		c.Default = c.Professions[0].Name
-	}
 
-	for _,p:=range c.Professions{
-		if p.Titles[]
+var (
+	defaultTitles = map[apinto.ZoneName][]string{
+		apinto.ZhCn: {"名称", "驱动", "创建时间", "更新时间"},
+		apinto.EnUs: {"name", "driver", "create time", "update time"},
 	}
+	defaultFields = []string{"name", "driver", "create", "update"}
+)
 
+func titleView(titles map[string][]string, fields []string) (map[apinto.ZoneName][]string, []string) {
+
+	if titles == nil && len(titles) == 0 {
+		return defaultTitles, defaultFields
+	}
+	titlesMap := make(map[apinto.ZoneName][]string)
+	for name, list := range titles {
+		titles[strings.ToLower(name)] = list
+	}
+	for zn, dv := range defaultTitles {
+		if v, has := titles[string(zn)]; !has || len(v) == 0 {
+			titlesMap[zn] = dv
+		} else {
+			titlesMap[zn] = v
+		}
+	}
+	if len(fields) == 0 {
+		fields = defaultFields
+	}
+	return titlesMap, fields
 }
 func toModule(c *Config) []*apinto.Module {
 	r := make([]*apinto.Module, 0, len(c.Professions))
 	for _, cm := range c.Professions {
+		titles, fields := titleView(cm.Titles, cm.Fields)
 		m := &apinto.Module{
 			Path:     fmt.Sprintf("/%s/list", cm.Name),
-			Handler:  professions.NewProfession(cm.Name, cm.Profession, nil, nil),
+			Handler:  professions.NewProfession(cm.Name, cm.Profession, titles, fields, nil),
 			Name:     cm.Name,
 			I18nName: make(map[apinto.ZoneName]string),
 		}
