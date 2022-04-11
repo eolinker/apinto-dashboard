7b49bdff3a2d7ff7a6b0fe29dff040ca15063292
吴泽楷 <2112005011@mail2.gdut.edu.cn>
2022-04-11 16:43:58 +0800

    页面确认弹出框封装

--- builds/static/js/dashboard.js
+++ builds/static/js/dashboard.js
@@ -34,3 +34,41 @@ let http = {
         });
     },
 }
+let confirm = function (title, msg, success, cancel){
+    let model = $("#confirmModel")
+    if (model.length > 0) {
+        model.remove();
+    }
+    let html = `<div class="modal fade" id="confirmModel" tabindex="-1" aria-labelledby="confirmModelLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="confirmModelLabel">`+title+`</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">`+msg+`</div>
+                        <div class="modal-footer">
+                            <button type="button" id="cancel_btn" class="btn btn-secondary">取消</button>
+                            <button type="button" id="confirm_btn" class="btn btn-danger" >删除</button>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+    $("body").append(html);
+    model = $("#confirmModel")
+    model.modal("show");
+    model.on("click", "button#cancel_btn",function() {
+        if (cancel) {
+            cancel()
+        }
+        $("#confirmModel").modal("hide");
+    });
+    model.on("click", "button#confirm_btn",function() {
+        if (success) {
+            success()
+        }
+        $("#confirmModel").modal("hide");
+    });
+}
