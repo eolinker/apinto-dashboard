f7ad3cbcbf2bab26cb3f8739c5f55e193f804b5f
chenjiekun <chenjiekun@sina.cn>
2022-04-14 19:12:31 +0800

    完成数据层

--- modules/activity-log/database/dao/dao.go
+++ modules/activity-log/database/dao/dao.go
@@ -1,6 +1,6 @@
 package dao
 
 type ActivityLogDao interface {
-	GetLogList(page, pageSize int) ([]*LogEntity, int64)
+	GetLogList(page, pageSize int) ([]*LogEntity, int64, error)
 	InsertLog(user, content, operation, object string, args []*Arg) error
 }
