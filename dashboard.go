3bb06055bb716f2101bb8ebfbfcf1203349905e8
黄孟柱 <hmzzrcs@163.com>
2022-04-22 14:46:01 +0800

    提交

--- dashboard.go
+++ dashboard.go
@@ -27,6 +27,7 @@ type Config struct {
 	DefaultZone        ZoneName
 	Modules            []*Module
 	UserDetailsService IUserDetailsService
+	ActivityLog        interface{}
 	Statics            map[string]string
 	DefaultModule      string
 }
