cef7fce60ebb5c78b42ce391bdc5f6912b063434
黄孟柱 <hmzzrcs@163.com>
2022-04-25 11:54:09 +0800

    render 转换

---
+++ builds/static/js/render.js
@@ -0,0 +1,18 @@
+(function( $ ){
+
+    $.fn.formRender = function(schema,data, options ) {
+
+        // Create some defaults, extending them with any options that were provided
+        var settings = $.extend( {
+            'location'         : 'top',
+            'background-color' : 'blue'
+        }, options);
+
+        return this.each(function() {
+
+            // Tooltip plugin code here
+
+        });
+
+    };
+})( jQuery );
