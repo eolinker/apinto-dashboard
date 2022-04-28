46d014a4a988ebdca2a7b4ea067d99d211365909
chenjiekun <chenjiekun@sina.cn>
2022-04-28 18:09:16 +0800

    finish function

--- app/apinto-dashboard/account.go
+++ app/apinto-dashboard/account.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"errors"
+	"github.com/eolinker/apinto-dashboard/internal/security"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+var (
+	ErrNilUserDetails = errors.New("config.yaml needs user_details. ")
+	ErrFileType       = errors.New("config.yaml user_details.tyoe: only file valid. ")
+	ErrNilAccountCfg  = errors.New("config.yaml user_details.file: account config is nil. ")
+)
+
 type Account struct {
 	AccountList []*AccountItem `yaml:"account_list"`
 }
@@ -10,6 +23,41 @@ type AccountItem struct {
 	Info     map[string]interface{} `yaml:"info"`
 }
 
-func InitUserDetails() {
+func InitUserDetails(detailsService *security.UserDetailsService, config *UserDetailsConfig) error {
+	if config == nil {
+		return ErrNilUserDetails
+	}
+	//暂时只支持file
+	if config.Type != "file" {
+		return ErrFileType
+	}
+	accountCfg, err := ReadAccountConfig(config.File)
+	if err != nil {
+		return err
+	}
 
+	if accountCfg == nil || len(accountCfg.AccountList) == 0 {
+		return ErrNilAccountCfg
+	}
+
+	for _, account := range accountCfg.AccountList {
+		detailsService.Add(security.NewUserDetails(account.UserName, account.Password, account.Info))
+	}
+
+	return nil
+}
+
+func ReadAccountConfig(file string) (*Account, error) {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	c := new(Account)
+	err = yaml.Unmarshal(data, c)
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
 }
