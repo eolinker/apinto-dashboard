7b87a2b8d3858ff967ea38db32c84dbf6ee2fdc3
Mengjie Yang <yangmengjie@eolink.com>
2023-03-24 16:18:19 +0800

    fix:修改拖拽接口

--- frontend/projects/core/src/app/layout/deploy/cluster/list/list.component.ts
+++ frontend/projects/core/src/app/layout/deploy/cluster/list/list.component.ts
@@ -1,8 +1,8 @@
 /* eslint-disable dot-notation */
 /*
- * @Author: MengjieYang yangmengjie@eolink.com
+ * @Author:
  * @Date: 2022-07-20 22:34:58
- * @LastEditors: MengjieYang yangmengjie@eolink.com
+ * @LastEditors:
  * @LastEditTime: 2022-09-28 00:20:26
  * @FilePath: /apinto/src/app/layout/deploy/deploy-cluster-list/deploy-cluster-list.component.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
