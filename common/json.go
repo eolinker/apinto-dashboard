246d884f9638ab8571d4be76247fe88fb6be4740
zhangzeyi <zhangzeyi@eolink.com>
2023-03-27 17:32:08 +0800

    jsonSchema格式校验返回具体错误信息

--- common/json.go
+++ common/json.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/xeipuuv/gojsonschema"
 	"io"
 )
@@ -11,19 +12,22 @@ func DecodeJSON(r io.Reader, obj any) error {
 	return decoder.Decode(obj)
 }
 
-func JsonSchemaValid(schema, data string) bool {
+func JsonSchemaValid(schema, data string) error {
 	// 解析JSON Schema
 	loader := gojsonschema.NewStringLoader(schema)
 	schemaDocument, err := gojsonschema.NewSchema(loader)
 	if err != nil {
-		return false
+		return err
 	}
 
 	// 解析待校验的JSON数据
 	documentLoader := gojsonschema.NewStringLoader(data)
 	result, err := schemaDocument.Validate(documentLoader)
 	if err != nil {
-		return false
+		return err
 	}
-	return result.Valid()
+	if result.Valid() {
+		return nil
+	}
+	return errors.New(result.Errors()[0].String())
 }
