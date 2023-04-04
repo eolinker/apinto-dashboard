27bd735a57d15a7cba9bf128b1ee425710b751a9
huangmengzhu <hmzzrcs@163.com>
2023-04-04 17:02:22 +0800

    驱动化

---
+++ app/apserver/driver.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	cluster_controller "github.com/eolinker/apinto-dashboard/modules/cluster/cluster-controller"
+	apinto_module "github.com/eolinker/apinto-module"
+)
+
+func init() {
+	apinto_module.Register("cluster.apinto.eolink.com", cluster_controller.NewClusterPlugin())
+}
