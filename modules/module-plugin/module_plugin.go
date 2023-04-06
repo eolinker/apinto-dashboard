3a0f2a0d859bcfd4f1683b40d1dd63441e5a91bb
chenjiekun <chenjiekun@sina.cn>
2023-04-06 21:34:10 +0800

    完成controller

--- modules/module-plugin/module_plugin.go
+++ modules/module-plugin/module_plugin.go
@@ -9,7 +9,7 @@ import (
 type IModulePluginService interface {
 	GetPlugins(ctx context.Context, groupUUID, searchName string) ([]*model.ModulePluginItem, []*model.PluginGroup, error)
 	GetPluginInfo(ctx context.Context, pluginUUID string) (*model.ModulePluginInfo, error)
-	GetPluginGroupsEnum(ctx context.Context) (*model.PluginGroup, error)
+	GetPluginGroupsEnum(ctx context.Context) ([]*model.PluginGroup, error)
 	GetPluginEnableInfo(ctx context.Context, pluginUUID string) (*model.PluginEnableInfo, *model.PluginEnableRender, error)
 	InstallPlugin(ctx context.Context, groupName string, packageContent []byte) error
 	EnablePlugin(ctx context.Context, pluginUUID string, enableInfo *dto.PluginEnableInfo) error
