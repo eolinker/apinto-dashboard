3556987b521846811497c29cb6b369473c855f7e
huangmengzhu <hmzzrcs@163.com>
2023-04-06 14:16:11 +0800

    提交

--- initialize/plugins.g.go
+++ initialize/plugins.g.go
@@ -1,7 +1,16 @@
 package initialize
 
-import "github.com/eolinker/apinto-dashboard/store"
+import (
+	"embed"
+	"github.com/eolinker/apinto-dashboard/store"
+)
+
+var (
+	//go:embed plugins
+	pluginDir embed.FS
+)
 
 func InitPlugins(idb store.IDB) {
 	// todo 初始化插件
+	pluginDir.ReadDir(".")
 }
