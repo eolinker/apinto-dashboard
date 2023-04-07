87d10b7835d8472095f6612b878eafba1c4eb0c8
huangmengzhu <hmzzrcs@163.com>
2023-04-07 19:01:56 +0800

    rebuild

--- modules/core/service/service.go
+++ modules/core/service/service.go
@@ -1,10 +1,13 @@
 package service
 
 import (
+	"context"
 	"github.com/eolinker/apinto-dashboard/modules/core"
 	"github.com/eolinker/apinto-dashboard/modules/middleware"
 	module_plugin "github.com/eolinker/apinto-dashboard/modules/module-plugin"
+	apinto_module "github.com/eolinker/apinto-module"
 	"github.com/eolinker/eosc/common/bean"
+	"github.com/eolinker/eosc/log"
 	"net/http"
 	"sync"
 	"sync/atomic"
@@ -22,6 +25,7 @@ type coreService struct {
 	middlewareService   middleware.IMiddlewareService
 	modulePluginService module_plugin.IModulePluginService
 	engineCreate        core.EngineCreate
+	providerService     core.IProviders
 }
 
 func (c *coreService) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -46,16 +50,69 @@ func (c *coreService) ReloadModule(version string) error {
 	if localVersion != nil && (*localVersion) == version {
 		// todo load module
 		// todo load middleware
-		c.rebuild()
+		err := c.rebuild()
+		if err != nil {
+			log.Error("error to rebuild core:", err)
+			return err
+		}
 	}
 	return nil
 }
-func (c *coreService) rebuild() {
-	c.engineCreate.CreateEngine()
-}
-func NewService() core.ICore {
+func (c *coreService) rebuild() error {
+	ctx := context.Background()
+	modules, err := c.modulePluginService.GetEnabledPlugins(ctx)
+	if err != nil {
+		return err
+	}
+	middlewaresConfig, err := c.middlewareService.Groups(ctx)
+	if err != nil {
+		return err
+	}
+	builder := apinto_module.NewModuleBuilder(c.engineCreate.CreateEngine())
+	for _, module := range modules {
+		driver, has := apinto_module.GetDriver(module.Driver)
+		if !has {
+			log.Errorf("not find driver:%s", module.Driver)
+			continue
+		}
+		plugin, err := driver.CreatePlugin(module.Define)
+		if err != nil {
+			log.Errorf("create plugin %s error:%s", module.Name, err.Error())
+			continue
+		}
+		err = plugin.CheckConfig(module.Name, module.Config.APIGroup, module.Config)
+		if err != nil {
+			log.Errorf("plugin module %s config error:%s", module.Name, err.Error())
+			continue
+		}
 
-	c := &coreService{}
+		m, err := plugin.CreateModule(module.Name, module.Config.APIGroup, module.Config)
+		if err != nil {
+			log.Errorf("create module %s  error:%s", module.Name, err.Error())
+			continue
+		}
+		builder.Append(m)
+	}
+	middlewares := make([]apinto_module.MiddlewareConfig, 0, len(middlewaresConfig))
+	for _, mid := range middlewaresConfig {
+		middlewares = append(middlewares, apinto_module.MiddlewareConfig{
+			Prefix:   mid.Prefix,
+			Handlers: mid.Middlewares,
+		})
+	}
+	handler, provider, err := builder.Build(middlewares)
+	if err != nil {
+		return err
+	}
+	c.handlerPointer.Store(&handler)
+	c.providerService.Set(provider)
+	return nil
+}
+func NewService(providerService core.IProviders) core.ICore {
+
+	c := &coreService{
+		providerService: providerService,
+	}
 	bean.Autowired(&c.modulePluginService)
 	bean.Autowired(&c.middlewareService)
 	bean.Autowired(&c.engineCreate)
