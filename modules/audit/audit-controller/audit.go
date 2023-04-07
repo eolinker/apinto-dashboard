bf81da71185f167a71361e59e20ed3c93394cf55
huangmengzhu <hmzzrcs@163.com>
2023-04-07 15:51:41 +0800

    提交

---
+++ modules/audit/audit-controller/audit.go
@@ -0,0 +1,71 @@
+package audit_controller
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/eolinker/apinto-dashboard/controller"
+	"github.com/eolinker/apinto-dashboard/enum"
+	"github.com/eolinker/apinto-dashboard/modules/base/namespace-controller"
+	"github.com/eolinker/eosc/log"
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+)
+
+func (a *auditLogController) Handler(ginCtx *gin.Context) {
+	start := time.Now()
+	bodyReader := ginCtx.Request.Body
+	bodyBytes, err := io.ReadAll(bodyReader)
+	bodyReader.Close()
+	if err != nil {
+		log.Warn("read body :", err)
+		ginCtx.JSON(http.StatusOK, controller.NewNoAccessError("Invalid request body"))
+		return
+	}
+	ginCtx.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+	//将请求体加入到上下文中，兼容需要修改的情况， 比如批量上线接口
+	ginCtx.Set(controller.LogBody, string(bodyBytes))
+
+	ginCtx.Next()
+
+	kind := ginCtx.GetString(controller.AuditKind)
+	operate := ginCtx.GetInt(controller.Operate)
+	//特殊情况. 通用分组controller暂只支持api分组写入审计日志
+	if kind == enum.LogKindCommonGroup {
+		groupType := ginCtx.Param("group_type")
+		if groupType != "api" {
+			return
+		}
+		kind = enum.LogKindAPIGroup
+	}
+
+	end := time.Now()
+	namespaceId := namespace_controller.GetNamespaceId(ginCtx)
+	userId := controller.GetUserId(ginCtx)
+
+	//获取ip
+	ip := ""
+	if forwarded := ginCtx.GetHeader("X-Forwarded-For"); len(forwarded) > 0 {
+		if i := strings.Index(forwarded, ","); i > 0 {
+			ip = forwarded[:i]
+		} else {
+			ip = forwarded
+		}
+	} else if realIP := ginCtx.GetHeader("X-Real-Ip"); len(realIP) > 0 {
+		ip = realIP
+	} else {
+		ip = ginCtx.ClientIP()
+	}
+
+	userAgent := ginCtx.GetHeader("user-agent")
+
+	errInfo := ginCtx.GetString(controller.ErrorCode)
+	//logger中间件将*ResponseWriterWrapper赋值给了ginCtx.Writer，这里是可断言的
+
+	url := fmt.Sprintf("%s %s", ginCtx.Request.Method, ginCtx.Request.RequestURI)
+
+	a.auditLogService.Log(namespaceId, userId, operate, kind, url, ginCtx.GetString("auditObject"), ip, userAgent, ginCtx.GetString(controller.LogBody), errInfo, start, end)
+
+}
