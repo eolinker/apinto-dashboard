812f48b6f304a0540d6ad47c53cd2850006c1ccf
huangmengzhu <hmzzrcs@163.com>
2023-04-07 15:51:05 +0800

    统一handler的审计日志的交互

--- controller/filters.go
+++ controller/filters.go
@@ -1,18 +1,7 @@
 package controller
 
 import (
-	"bytes"
-	"context"
-	"errors"
-	"fmt"
-	"github.com/eolinker/apinto-dashboard/common"
-	"github.com/eolinker/eosc/log"
 	"github.com/gin-gonic/gin"
-	"github.com/go-basic/uuid"
-	"io"
-	"net/http"
-	"strings"
-	"time"
 )
 
 const (
@@ -20,26 +9,12 @@ const (
 	Authorization  = "Authorization"
 	Session        = "Session"
 	RAuthorization = "RAuthorization"
+	ErrorCode      = "ErrorCode"
+	ErrorMessage   = "ErrorBody"
+	Operate        = "Operate"
+	AuditKind      = "AuditKind"
+	LogBody
 )
-
-var loginError = errors.New("登录已失效，请重新登录")
-
-type IFilterRouter interface {
-	MustNamespace(*gin.Context)
-	VerifyToken(*gin.Context)
-}
-
-func VerifyToken(ginCtx *gin.Context) {
-
-	session, _ := ginCtx.Cookie(Session)
-	if session == "" {
-		session = uuid.New()
-		ginCtx.SetCookie(Session, session, 0, "", "", false, true)
-
-	}
-
-	ginCtx.Set(UserId, 1)
-}
 
 func GetUserId(ginCtx *gin.Context) int {
 	return ginCtx.GetInt(UserId)
@@ -52,96 +27,15 @@ func GenAccessHandler() gin.HandlerFunc {
 	}
 }
 
-func Recovery(c *gin.Context) {
-	defer func() {
-		if err := recover(); err != nil {
-			log.Errorf("panic %v", common.PanicTrace(err))
-			fmt.Println(common.PanicTrace(err))
-			c.JSON(http.StatusInternalServerError, NewErrorResult("服务器内部错误"))
-		}
-	}()
+func ErrorJsonWithCode(ginCtx *gin.Context, statusCode int, errorCode int, errorMsg string) {
+	ginCtx.Set(ErrorMessage, errorMsg)
+	ginCtx.Set(ErrorCode, errorCode)
+	ginCtx.JSON(statusCode, &Result{
+		Code: errorCode,
+		Msg:  errorMsg,
+	})
 
-	c.Next()
 }
-
-type ResponseWriterWrapper struct {
-	gin.ResponseWriter
-	Body *bytes.Buffer // 缓存
-}
-
-func (w ResponseWriterWrapper) Write(b []byte) (int, error) {
-	w.Body.Write(b)
-	return w.ResponseWriter.Write(b)
-}
-
-func (w ResponseWriterWrapper) WriteString(s string) (int, error) {
-	w.Body.WriteString(s)
-	return w.ResponseWriter.WriteString(s)
-}
-
-func Logger(ginCtx *gin.Context) {
-	if !strings.HasPrefix(ginCtx.Request.URL.Path, "/api/") {
-		ginCtx.Next()
-		return
-	}
-
-	ginCtx.Request = ginCtx.Request.WithContext(context.Background())
-
-	data, err := ginCtx.GetRawData()
-	if err == nil {
-		ginCtx.Request.Body = io.NopCloser(bytes.NewBuffer(data)) // 关键点
-	}
-
-	blw := &ResponseWriterWrapper{Body: bytes.NewBufferString(""), ResponseWriter: ginCtx.Writer}
-	ginCtx.Writer = blw
-
-	// 开始时间
-	startTime := time.Now()
-
-	// 处理请求
-	ginCtx.Next()
-
-	// 结束时间
-	endTime := time.Now()
-
-	// 执行时间
-	latencyTime := endTime.Sub(startTime)
-
-	// 请求方式
-	reqMethod := ginCtx.Request.Method
-
-	// 请求路由
-	reqUri := ginCtx.Request.RequestURI
-
-	// 状态码
-	statusCode := ginCtx.Writer.Status()
-
-	// 请求IP
-	clientIP := ginCtx.ClientIP()
-	resBody := blw.Body.String()
-	//日志格式
-	log.DebugF("| %3d | %13v | %15s | %s | %s | reqBody=%s | resBody=%s ",
-		statusCode,
-		latencyTime,
-		clientIP,
-		reqMethod,
-		reqUri,
-		string(data),
-		resBody,
-	)
-}
-
-type CustomResponseWriter struct {
-	gin.ResponseWriter
-	body *bytes.Buffer
-}
-
-func (w CustomResponseWriter) Write(b []byte) (int, error) {
-	w.body.Write(b)
-	return w.ResponseWriter.Write(b)
-}
-
-func (w CustomResponseWriter) WriteString(s string) (int, error) {
-	w.body.WriteString(s)
-	return w.ResponseWriter.WriteString(s)
+func ErrorJson(ginCtx *gin.Context, statusCode int, errorMsg string) {
+	ErrorJsonWithCode(ginCtx, statusCode, ordinaryCode, errorMsg)
 }
