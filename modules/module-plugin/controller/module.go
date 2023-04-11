d3f65cdd239f6c589d3fb218f56c0fe52dfd1cc9
chenjiekun <chenjiekun@sina.cn>
2023-04-11 16:59:16 +0800

    完成插件管理的module

--- modules/module-plugin/controller/module.go
+++ modules/module-plugin/controller/module.go
@@ -1,153 +1,144 @@
 package controller
 
-//
-//type ModulePluginDriver struct {
-//}
-//
-//func NewModulePlugin() apinto_module.Driver {
-//	return &ModulePluginDriver{}
-//}
-//
-//const (
-//	ModuleView = "view"
-//	ModuleEdit = "edit"
-//)
-//
-//func (c *ModulePluginDriver) CreateModule(name string, config interface{}) (apinto_module.Module, error) {
-//	return NewModulePluginModule(name), nil
-//}
-//
-//func (c *ModulePluginDriver) CheckConfig(name string, config interface{}) error {
-//	return nil
-//}
-//
-//func (c *ModulePluginDriver) CreatePlugin(define interface{}) (apinto_module.Plugin, error) {
-//	return c, nil
-//}
-//
-//type ModulePluginModule struct {
-//	isInit  bool
-//	name    string
-//	routers apinto_module.RoutersInfo
-//}
-//
-//func (c *ModulePluginModule) Name() string {
-//	return c.name
-//}
-//
-//func (c *ModulePluginModule) Support() (apinto_module.ProviderSupport, bool) {
-//	return nil, false
-//}
-//
-//func (c *ModulePluginModule) Routers() (apinto_module.Routers, bool) {
-//	return c, true
-//}
-//
-//func (c *ModulePluginModule) Middleware() (apinto_module.Middleware, bool) {
-//	return nil, false
-//}
-//
-//func NewModulePluginModule(name string) *ModulePluginModule {
-//
-//	return &ModulePluginModule{name: name}
-//}
-//
-//func (c *ModulePluginModule) RoutersInfo() apinto_module.RoutersInfo {
-//	if !c.isInit {
-//		c.initRouter()
-//		c.isInit = true
-//	}
-//	return c.routers
-//}
-//
-//func (c *ModulePluginModule) initRouter() {
-//	clrController := newClusterController()
-//	nodeController := newClusterNodeController()
-//	configController := newClusterConfigController()
-//
-//	c.routers = []apinto_module.RouterInfo{
-//		{
-//			Method:      http.MethodGet,
-//			Path:        "/api/cluster/lists",
-//			Handler:     "cluster.list",
-//			HandlerFunc: []apinto_module.HandlerFunc{clrController.clusters},
-//		},
-//		{
-//			Method:      http.MethodGet,
-//			Path:        "/api/cluster/enum",
-//			Handler:     "cluster.enum",
-//			HandlerFunc: []apinto_module.HandlerFunc{clrController.clusterEnum},
-//		},
-//		{
-//			Method:      http.MethodGet,
-//			Path:        "/api/cluster",
-//			Handler:     "cluster.info",
-//			HandlerFunc: []apinto_module.HandlerFunc{clrController.cluster},
-//		},
-//		{
-//			Method:      http.MethodDelete,
-//			Path:        "/api/cluster",
-//			Handler:     "cluster.delete",
-//			HandlerFunc: []apinto_module.HandlerFunc{clrController.del},
-//		}, {
-//			Method:      http.MethodPost,
-//			Path:        "/api/cluster",
-//			Handler:     "cluster.create",
-//			HandlerFunc: []apinto_module.HandlerFunc{clrController.create},
-//		},
-//		{
-//			Method:      http.MethodGet,
-//			Path:        "/api/cluster-test",
-//			Handler:     "cluster.test",
-//			HandlerFunc: []apinto_module.HandlerFunc{clrController.test},
-//		},
-//		{
-//			Method:      http.MethodPut,
-//			Path:        "/api/cluster/:cluster_name/desc",
-//			Handler:     "cluster.desc.edit",
-//			HandlerFunc: []apinto_module.HandlerFunc{clrController.putDesc},
-//		},
-//		{
-//			Method:      http.MethodGet,
-//			Path:        "/api/cluster/:cluster_name/nodes",
-//			Handler:     "cluster.nodes",
-//			HandlerFunc: []apinto_module.HandlerFunc{nodeController.nodes},
-//		},
-//		{
-//			Method:      http.MethodPost,
-//			Path:        "/api/cluster/:cluster_name/node/reset",
-//			Handler:     "cluster.nodes.reset",
-//			HandlerFunc: []apinto_module.HandlerFunc{nodeController.reset},
-//		},
-//		{
-//			Method:      http.MethodPut,
-//			Path:        "/api/cluster/:cluster_name/node",
-//			Handler:     "cluster.nodes.edit",
-//			HandlerFunc: []apinto_module.HandlerFunc{nodeController.put},
-//		},
-//		{
-//			Method:      http.MethodGet,
-//			Path:        "/api/cluster/:cluster_name/configuration/:type",
-//			Handler:     "cluster.config",
-//			HandlerFunc: []apinto_module.HandlerFunc{configController.get},
-//		},
-//		{
-//			Method:      http.MethodPut,
-//			Path:        "/api/cluster/:cluster_name/configuration/:type",
-//			Handler:     "cluster.config.edit",
-//			HandlerFunc: []apinto_module.HandlerFunc{configController.edit},
-//		},
-//		{
-//			Method:      http.MethodPut,
-//			Path:        "/api/cluster/:cluster_name/configuration/:type/enable",
-//			Handler:     "cluster.config.enable",
-//			HandlerFunc: []apinto_module.HandlerFunc{configController.enable},
-//		},
-//		{
-//			Method:      http.MethodPut,
-//			Path:        "/api/cluster/:cluster_name/configuration/:type/disable",
-//			Handler:     "cluster.config.disable",
-//			HandlerFunc: []apinto_module.HandlerFunc{configController.disable},
-//		},
-//	}
-//}
+import (
+	apinto_module "github.com/eolinker/apinto-module"
+	"net/http"
+)
+
+type ModulePluginDriver struct {
+}
+
+func NewModulePlugin() apinto_module.Driver {
+	return &ModulePluginDriver{}
+}
+
+const (
+	ModulePluginView = "view"
+	ModulePluginEdit = "edit"
+)
+
+func (c *ModulePluginDriver) CreateModule(name string, config interface{}) (apinto_module.Module, error) {
+	return NewModulePluginModule(name), nil
+}
+
+func (c *ModulePluginDriver) CheckConfig(name string, config interface{}) error {
+	return nil
+}
+
+func (c *ModulePluginDriver) CreatePlugin(define interface{}) (apinto_module.Plugin, error) {
+	return c, nil
+}
+
+type ModulePluginModule struct {
+	isInit  bool
+	name    string
+	routers apinto_module.RoutersInfo
+}
+
+func (c *ModulePluginModule) Name() string {
+	return c.name
+}
+
+func (c *ModulePluginModule) Support() (apinto_module.ProviderSupport, bool) {
+	return nil, false
+}
+
+func (c *ModulePluginModule) Routers() (apinto_module.Routers, bool) {
+	return c, true
+}
+
+func (c *ModulePluginModule) Middleware() (apinto_module.Middleware, bool) {
+	return nil, false
+}
+
+func NewModulePluginModule(name string) *ModulePluginModule {
+
+	return &ModulePluginModule{name: name}
+}
+
+func (c *ModulePluginModule) RoutersInfo() apinto_module.RoutersInfo {
+	if !c.isInit {
+		c.initRouter()
+		c.isInit = true
+	}
+	return c.routers
+}
+
+func (c *ModulePluginModule) initRouter() {
+	mPluginController := newModulePluginController()
+	controllerPluginFront := newPluginFrontController()
+	c.routers = []apinto_module.RouterInfo{
+		{
+			Method:      http.MethodGet,
+			Path:        "/api/plugin/installed",
+			Handler:     "modulePlugin.plugins",
+			HandlerFunc: []apinto_module.HandlerFunc{mPluginController.plugins},
+		},
+		{
+			Method:      http.MethodGet,
+			Path:        "/api/plugin/info",
+			Handler:     "modulePlugin.getPluginInfo",
+			HandlerFunc: []apinto_module.HandlerFunc{mPluginController.getPluginInfo},
+		},
+		{
+			Method:      http.MethodGet,
+			Path:        "/api/plugin/groups/enum",
+			Handler:     "modulePlugin.getGroupsEnum",
+			HandlerFunc: []apinto_module.HandlerFunc{mPluginController.getGroupsEnum},
+		},
+		{
+			Method:      http.MethodGet,
+			Path:        "/api/plugin/enable",
+			Handler:     "modulePlugin.getEnableInfo",
+			HandlerFunc: []apinto_module.HandlerFunc{mPluginController.getEnableInfo},
+		},
+		{
+			Method:      http.MethodPost,
+			Path:        "/api/plugin/install",
+			Handler:     "modulePlugin.install",
+			HandlerFunc: []apinto_module.HandlerFunc{mPluginController.install},
+		},
+		{
+			Method:      http.MethodPost,
+			Path:        "/api/plugin/enable",
+			Handler:     "modulePlugin.enable",
+			HandlerFunc: []apinto_module.HandlerFunc{mPluginController.enable},
+		},
+		{
+			Method:      http.MethodPost,
+			Path:        "/api/plugin/disable",
+			Handler:     "modulePlugin.disable",
+			HandlerFunc: []apinto_module.HandlerFunc{mPluginController.disable},
+		},
+		{
+			Method:      http.MethodGet,
+			Path:        "/api/plugin/icon/:id/:file",
+			Handler:     "pluginFront.getPluginIcon",
+			HandlerFunc: []apinto_module.HandlerFunc{controllerPluginFront.checkPluginID, controllerPluginFront.setIConName, controllerPluginFront.getPluginInfo},
+		},
+		{
+			Method:      http.MethodGet,
+			Path:        "/api/plugin/icon/:id",
+			Handler:     "pluginFront.getPluginIconDefault",
+			HandlerFunc: []apinto_module.HandlerFunc{controllerPluginFront.checkPluginID, controllerPluginFront.setIConName, controllerPluginFront.getPluginInfo},
+		},
+		{
+			Method:      http.MethodGet,
+			Path:        "/api/plugin/md/:id/:file",
+			Handler:     "pluginFront.getPluginMD",
+			HandlerFunc: []apinto_module.HandlerFunc{controllerPluginFront.checkPluginID, controllerPluginFront.setMDName, controllerPluginFront.getPluginInfo},
+		},
+		{
+			Method:      http.MethodGet,
+			Path:        "/api/plugin/md/:id",
+			Handler:     "pluginFront.getPluginMDDefault",
+			HandlerFunc: []apinto_module.HandlerFunc{controllerPluginFront.checkPluginID, controllerPluginFront.setMDName, controllerPluginFront.getPluginInfo},
+		},
+		{
+			Method:      http.MethodGet,
+			Path:        "/api/plugin/info/:id/resources/*filepath",
+			Handler:     "pluginFront.getPluginResources",
+			HandlerFunc: []apinto_module.HandlerFunc{controllerPluginFront.checkPluginID, controllerPluginFront.getPluginResources},
+		},
+	}
+}
