8b332e5d4312b9ba6f2e6d19786c5c423e0fc0f9
Liujian <824010343@qq.com>
2023-04-11 15:23:54 +0800

    修改导航路由

--- app/apserver/driver.go
+++ app/apserver/driver.go
@@ -4,6 +4,7 @@ import (
 	audit_controller "github.com/eolinker/apinto-dashboard/modules/audit/audit-controller"
 	cluster_controller "github.com/eolinker/apinto-dashboard/modules/cluster/cluster-controller"
 	"github.com/eolinker/apinto-dashboard/modules/core/controller"
+	navigation_controller "github.com/eolinker/apinto-dashboard/modules/navigation/navigation-controller"
 	apinto_module "github.com/eolinker/apinto-module"
 )
 
@@ -11,5 +12,6 @@ func init() {
 
 	apinto_module.Register("core.apinto.com", controller.NewCoreDriver())
 	apinto_module.Register("cluster.apinto.com", cluster_controller.NewClusterPlugin())
+	apinto_module.Register("navigation.apinto.com", navigation_controller.NewNavigationPlugin())
 	apinto_module.Register("audit.apinto.com", audit_controller.NewDriver())
 }
