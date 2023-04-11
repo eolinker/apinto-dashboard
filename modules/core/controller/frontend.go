b4c6cf2eb7cf7412591a3b64e47951602cc4304d
huangmengzhu <hmzzrcs@163.com>
2023-04-11 15:04:45 +0800

    重新规划前端

--- modules/core/controller/frontend.go
+++ modules/core/controller/frontend.go
@@ -1,66 +1,34 @@
 package controller
 
 import (
-	"embed"
-	"github.com/eolinker/apinto-dashboard/common/gzip-static"
+	"github.com/eolinker/apinto-dashboard/frontend"
 	apinto_module "github.com/eolinker/apinto-module"
 	"github.com/gin-gonic/gin"
-	"io/fs"
 	"net/http"
 	"path"
 )
 
-//go:embed dist/index.html
-var indexContext []byte
-
-//go:embed dist
-var dist embed.FS
-
-//go:embed dist/favicon.ico
-var iconContext []byte
-
-func IndexHtml(ginCtx *gin.Context) {
-	ginCtx.Header("Cache-Control", "no-store, no-cache, max-age=0, must-revalidate, proxy-revalidate")
-	ginCtx.Data(http.StatusOK, "text/html; charset=utf-8", indexContext)
-	return
-}
 func indexRouter() apinto_module.RouterInfo {
 	return apinto_module.RouterInfo{
 		Method:      http.MethodGet,
 		Path:        "/",
 		Handler:     "index",
 		Labels:      apinto_module.RouterLabelModule,
-		HandlerFunc: []apinto_module.HandlerFunc{IndexHtml},
+		HandlerFunc: []apinto_module.HandlerFunc{frontend.IndexHtml},
 	}
 }
 func favicon() apinto_module.RouterInfo {
 	return apinto_module.RouterInfo{
-		Method:  http.MethodGet,
-		Path:    "/favicon.ico",
-		Handler: "favicon.ico",
-		Labels:  apinto_module.RouterLabelAssets,
-		HandlerFunc: []apinto_module.HandlerFunc{func(ginCtx *gin.Context) {
-			ginCtx.Data(http.StatusOK, "image/x-icon", iconContext)
-		}},
+		Method:      http.MethodGet,
+		Path:        "/favicon.ico",
+		Handler:     "favicon.ico",
+		Labels:      apinto_module.RouterLabelAssets,
+		HandlerFunc: []apinto_module.HandlerFunc{frontend.Favicon},
 	}
 }
-
-func getFileSystem(dir string) http.FileSystem {
-	fDir, err := fs.Sub(dist, dir)
-	if err != nil {
-		panic(err)
-	}
-
-	return http.FS(fDir)
-
-}
-
-var (
-	gzipHandler = gzip.Gzip(gzip.DefaultCompression)
-)
 
 func staticFile(prefix string, dir string) apinto_module.RoutersInfo {
-	fileSystem := getFileSystem(dir)
+	fileSystem := frontend.GetFileSystem(dir)
 	handler := func(ginCtx *gin.Context) {
 		filePath := ginCtx.Param("filepath")
 		ginCtx.FileFromFS(filePath, fileSystem)
@@ -72,14 +40,14 @@ func staticFile(prefix string, dir string) apinto_module.RoutersInfo {
 			Path:        path.Join(prefix, "/*filepath"),
 			Handler:     prefix,
 			Labels:      apinto_module.RouterLabelAssets,
-			HandlerFunc: []apinto_module.HandlerFunc{addExpires, gzipHandler, handler},
+			HandlerFunc: []apinto_module.HandlerFunc{frontend.AddExpires, frontend.GzipHandler, handler},
 		},
 		{
 			Method:      http.MethodHead,
 			Path:        path.Join(prefix, "/*filepath"),
 			Handler:     prefix,
 			Labels:      apinto_module.RouterLabelAssets,
-			HandlerFunc: []apinto_module.HandlerFunc{addExpires, gzipHandler, handler},
+			HandlerFunc: []apinto_module.HandlerFunc{frontend.AddExpires, frontend.GzipHandler, handler},
 		},
 	}
 }
