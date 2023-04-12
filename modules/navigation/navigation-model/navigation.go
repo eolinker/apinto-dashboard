f4e6fab4243b025d0d363d13d3ea1461251a19ba
Liujian <824010343@qq.com>
2023-04-12 15:39:33 +0800

    导航管理模块调整

--- modules/navigation/navigation-model/navigation.go
+++ modules/navigation/navigation-model/navigation.go
@@ -1,35 +1,22 @@
 package navigation_model
 
-type NavigationBasicInfo struct {
-	ID        int    `json:"-"`
-	Uuid      string `json:"uuid"`
-	Title     string `json:"title"`
-	Icon      string `json:"icon"`
-	IconType  string `json:"icon_type"`
-	CanDelete bool   `json:"can_delete"`
-	Sort      int    `json:"-"`
+type Navigation struct {
+	Uuid  string `json:"uuid"`
+	Title string `json:"title"`
+	Icon  string `json:"icon"`
+	Index int    `json:"-"`
 }
 
-type Navigations []*NavigationBasicInfo
+type Navigations []*Navigation
 
 func (n Navigations) Len() int {
 	return len(n)
 }
 
 func (n Navigations) Less(i, j int) bool {
-	return n[i].Sort < n[j].Sort
+	return n[i].Index < n[j].Index
 }
 
 func (n Navigations) Swap(i, j int) {
 	n[i], n[j] = n[j], n[i]
-}
-
-type Navigation struct {
-	*NavigationBasicInfo
-	Modules []*Module `json:"modules"`
-}
-
-type Module struct {
-	Id    string `json:"id"`
-	Title string `json:"title"`
 }
