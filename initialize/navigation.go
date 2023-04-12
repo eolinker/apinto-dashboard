2a1b94abd308e85e6b83ce1772556d442103f3fb
Liujian <824010343@qq.com>
2023-04-12 16:18:20 +0800

    初始化navigation修改

--- initialize/navigation.go
+++ initialize/navigation.go
@@ -1,13 +1,13 @@
 package initialize
 
 import (
-	"context"
 	_ "embed"
 
-	"github.com/eolinker/apinto-dashboard/modules/navigation"
 	"github.com/eolinker/eosc/common/bean"
+
+	navigation_model "github.com/eolinker/apinto-dashboard/modules/navigation/navigation-model"
+
 	"gopkg.in/yaml.v3"
-	"gorm.io/gorm"
 )
 
 var (
@@ -15,33 +15,26 @@ var (
 	navigationContent []byte
 )
 
-type NavigationCfg struct {
-	ID   string `yaml:"id"`
-	Name string `yaml:"name"`
-	ICON string `yaml:"icon"`
-}
-
 func InitNavigation() error {
 	// 初始化导航
-	var service navigation.INavigationService
-	bean.Autowired(&service)
-	ctx := context.Background()
-	navigations := make([]*NavigationCfg, 0)
+	navigations := make([]*navigation_model.Navigation, 0)
 	err := yaml.Unmarshal(navigationContent, &navigations)
 	if err != nil {
 		return err
 	}
-	for _, nav := range navigations {
-		_, err = service.GetIDByUUID(ctx, nav.ID)
-		if err != nil {
-			if err == gorm.ErrRecordNotFound {
-				err = service.Add(ctx, nav.ID, nav.Name, nav.ICON, "css")
-				if err != nil {
-					return err
-				}
-			}
-			return err
-		}
-	}
+	service := newNavigationDataService(navigations)
+	bean.Injection(&service)
 	return nil
+}
+
+type navigationDataService struct {
+	navigations []*navigation_model.Navigation
+}
+
+func newNavigationDataService(navigations []*navigation_model.Navigation) *navigationDataService {
+	return &navigationDataService{navigations: navigations}
+}
+
+func (n *navigationDataService) GetNavigationData() []*navigation_model.Navigation {
+	return n.navigations
 }
