215f48c9c75fcae68005714efd8b2ef70f1fc3c8
Liujian <824010343@qq.com>
2023-04-12 17:28:39 +0800

    导航栏完成

--- modules/core/model/navigation.go
+++ modules/core/model/navigation.go
@@ -1,11 +1,11 @@
 package model
 
 type Navigation struct {
-	Title    string    `json:"title"`
-	Icon     string    `json:"icon"`
-	IconType string    `json:"icon_type"`
-	Modules  []*Module `json:"modules"`
-	Default  string    `json:"default"`
+	Title string `json:"title"`
+	Icon  string `json:"icon"`
+	//IconType string    `json:"icon_type"`
+	Modules []*Module `json:"modules"`
+	Default string    `json:"default"`
 }
 
 type Module struct {
