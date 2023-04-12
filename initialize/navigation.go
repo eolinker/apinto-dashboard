3bfb72b7f4dcc86a14aa64c60b6542de1593188c
Liujian <824010343@qq.com>
2023-04-12 16:39:02 +0800

    初始化navigation修改

--- initialize/navigation.go
+++ initialize/navigation.go
@@ -3,6 +3,8 @@ package initialize
 import (
 	_ "embed"
 
+	"github.com/eolinker/apinto-dashboard/modules/navigation"
+
 	"github.com/eolinker/eosc/common/bean"
 
 	navigation_model "github.com/eolinker/apinto-dashboard/modules/navigation/navigation-model"
@@ -15,23 +17,22 @@ var (
 	navigationContent []byte
 )
 
-func InitNavigation() error {
+func init() {
 	// 初始化导航
 	navigations := make([]*navigation_model.Navigation, 0)
 	err := yaml.Unmarshal(navigationContent, &navigations)
 	if err != nil {
-		return err
+		panic(err)
 	}
 	service := newNavigationDataService(navigations)
 	bean.Injection(&service)
-	return nil
 }
 
 type navigationDataService struct {
 	navigations []*navigation_model.Navigation
 }
 
-func newNavigationDataService(navigations []*navigation_model.Navigation) *navigationDataService {
+func newNavigationDataService(navigations []*navigation_model.Navigation) navigation.INavigationDataService {
 	return &navigationDataService{navigations: navigations}
 }
 
