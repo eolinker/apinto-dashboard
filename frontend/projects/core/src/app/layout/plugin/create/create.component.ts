79bf3b7bcfb3147d49018114dd0af7639d4ce0bc
maggieyyy <61950669+maggieyyy@users.noreply.github.com>
2023-04-14 09:43:44 +0800

    frontend-fix:首页&覆盖md暗黑模式&安装插件后接口顺序

--- frontend/projects/core/src/app/layout/plugin/create/create.component.ts
+++ frontend/projects/core/src/app/layout/plugin/create/create.component.ts
@@ -7,6 +7,7 @@ import { defaultAutoTips } from '../../../constant/conf'
 import { EmptyHttpResponse } from '../../../constant/type'
 import { ApiService } from '../../../service/api.service'
 import { EoNgMessageService } from '../../../service/eo-ng-message.service'
+import { EoNgPluginService } from '../eo-ng-plugin.service'
 
 @Component({
   selector: 'eo-ng-plugin-create',
@@ -32,47 +33,48 @@ import { EoNgMessageService } from '../../../service/eo-ng-message.service'
               选择文件
             </button>
           </nz-upload>
-            <div
-              *ngIf="fileError"
-              class="ant-form-item-explain-error"
-              (click)="$event.stopPropagation()"
-            >
-              必填项
-            </div>
-            <div
-              class="ant-form-item-extra activation-extra"
-              style="padding-left: 0"
-              (click)="$event.stopPropagation()"
-            >
-              仅支持官方提供插件配置模板文件
-            </div>
+          <div
+            *ngIf="fileError"
+            class="ant-form-item-explain-error"
+            (click)="$event.stopPropagation()"
+          >
+            必填项
+          </div>
+          <div
+            class="ant-form-item-extra activation-extra"
+            style="padding-left: 0"
+            (click)="$event.stopPropagation()"
+          >
+            仅支持官方提供插件配置模板文件
+          </div>
         </nz-form-control>
       </nz-form-item>
-  <nz-form-item class="form-row">
-    <nz-form-label [nzSpan]="6" nzRequired>分组：</nz-form-label>
-    <nz-form-control [nzSpan]="14">
-      <eo-ng-auto-complete
+      <nz-form-item class="form-row">
+        <nz-form-label [nzSpan]="6" nzRequired>分组：</nz-form-label>
+        <nz-form-control [nzSpan]="14">
+          <eo-ng-auto-complete
             formControlName="name"
             [nzOptions]="groupOptions"
           ></eo-ng-auto-complete>
-    </nz-form-control>
-  </nz-form-item>
-</form>
+        </nz-form-control>
+      </nz-form-item>
+    </form>
   `,
-  styles: [
-  ]
+  styles: []
 })
 export class PluginCreateComponent {
   autoTips: Record<string, Record<string, string>> = defaultAutoTips
   validateForm: FormGroup = new FormGroup({})
-  fileError:boolean = false
-  fileList: NzUploadFile[] = [];
-  groupOptions:AutoCompleteOption[] = []
-  name= ''
-  constructor (
-    public api:ApiService,
+  fileError: boolean = false
+  fileList: NzUploadFile[] = []
+  groupOptions: AutoCompleteOption[] = []
+  name = ''
+  constructor(
+    public api: ApiService,
     private fb: UntypedFormBuilder,
-    private message:EoNgMessageService) {
+    private message: EoNgMessageService,
+    private service: EoNgPluginService
+  ) {
     this.validateForm = this.fb.group({
       file: [null, [Validators.required]],
       name: ['', [Validators.required]]
@@ -80,14 +82,24 @@ export class PluginCreateComponent {
     this.getGroupList()
   }
 
-  getGroupList () {
-    this.api.get('system/plugin/groups/enum').subscribe((resp:{code:number, data:{groups:Array<{uuid:string, name:string}>}, msg:string}) => {
-      if (resp.code === 0) {
-        this.groupOptions = resp.data.groups.map((group:{uuid:string, name:string}) => {
-          return { label: group.name, value: group.name }
-        })
-      }
-    })
+  getGroupList() {
+    this.api
+      .get('system/plugin/groups/enum')
+      .subscribe(
+        (resp: {
+          code: number
+          data: { groups: Array<{ uuid: string; name: string }> }
+          msg: string
+        }) => {
+          if (resp.code === 0) {
+            this.groupOptions = resp.data.groups.map(
+              (group: { uuid: string; name: string }) => {
+                return { label: group.name, value: group.name }
+              }
+            )
+          }
+        }
+      )
   }
 
   // 手动上传文件
@@ -99,37 +111,50 @@ export class PluginCreateComponent {
   }
 
   // 移除文件
-  removeFile () {
+  removeFile() {
     this.fileList = []
     this.fileError = true
     return true
   }
 
-  checkValid () {
-    if (this.fileList.length === 0 || !this.validateForm.controls['name'].valid) {
+  checkValid() {
+    if (
+      this.fileList.length === 0 ||
+      !this.validateForm.controls['name'].valid
+    ) {
       this.fileError = this.fileList.length === 0
       if (this.validateForm.controls['name'].invalid) {
         this.validateForm.controls['name'].markAsDirty()
-        this.validateForm.controls['name'].updateValueAndValidity({ onlySelf: true })
+        this.validateForm.controls['name'].updateValueAndValidity({
+          onlySelf: true
+        })
       }
       return false
     }
     return true
   }
 
-  submit () {
-    if (!this.checkValid()) { return false }
+  submit() {
+    if (!this.checkValid()) {
+      return false
+    }
     const formData = new FormData()
     formData.append('plugin', this.fileList[0] as any)
     // eslint-disable-next-line dot-notation
-    formData.append('group_name', this.validateForm.controls['name'].value as any)
-    return this.api.post('system/plugin/install', formData).subscribe((resp:EmptyHttpResponse) => {
-      if (resp.code === 0) {
-        this.message.success(resp.msg || '安装插件成功')
-        return true
-      } else {
-        return false
-      }
-    })
+    formData.append(
+      'group_name',
+      this.validateForm.controls['name'].value as any
+    )
+    return this.api
+      .post('system/plugin/install', formData)
+      .subscribe((resp: EmptyHttpResponse) => {
+        if (resp.code === 0) {
+          this.message.success(resp.msg || '安装插件成功')
+          this.service.getPluginList()
+          return true
+        } else {
+          return false
+        }
+      })
   }
 }
--- frontend/projects/core/src/app/layout/plugin/message/message.component.ts
+++ frontend/projects/core/src/app/layout/plugin/message/message.component.ts
@@ -1,110 +1,156 @@
 import { Component, Inject, OnInit } from '@angular/core'
 import { EoNgFeedbackModalService } from 'eo-ng-feedback'
 import { NzModalRef } from 'ng-zorro-antd/modal'
-import { MODAL_NORMAL_SIZE, MODAL_SMALL_SIZE } from '../../../constant/app.config'
+import {
+  MODAL_NORMAL_SIZE,
+  MODAL_SMALL_SIZE
+} from '../../../constant/app.config'
 import { EmptyHttpResponse } from '../../../constant/type'
 import { API_URL, ApiService } from '../../../service/api.service'
 import { BaseInfoService } from '../../../service/base-info.service'
 import { EoNgMessageService } from '../../../service/eo-ng-message.service'
 import { EoNgNavigationService } from '../../../service/eo-ng-navigation.service'
 import { PluginConfigComponent } from '../config/config.component'
-import { PluginInstallConfigData, PluginInstallData, PluginMessage } from '../types/types'
+import {
+  PluginInstallConfigData,
+  PluginInstallData,
+  PluginMessage
+} from '../types/types'
 import { MarkdownService } from 'ngx-markdown'
 import { Router } from '@angular/router'
 
 @Component({
   selector: 'eo-ng-plugin-message',
   template: `
-  <header class="mx-[40px] my-[20px]">
-    <div class="flex justify-between  mb-btnybase items-center">
-      <div class="flex">
-        <div class="mr-btnrbase w-[50px] h-[50px]">
-            <img class="mr-btnrbase" [src]="icon" alt="icon" width="50px" height="50px">
-      </div>
-        <div class="flex flex-col justify-around">
-          <p class="text-[14px] font-medium">{{title}}</p>
-          <p>
-
-          <span class="mr-[8px] font-medium text-[#0A89FF] bg-[#0A89FF1A] px-[4px] py-[2px] leading-[20px] rounded" *ngIf="enable">已启用</span>
-                <span  class="mr-[8px] font-medium text-[#bbbbbb] bg-[#bbbbbb1A] px-[4px] py-[2px] leading-[20px] rounded" *ngIf="!enable"> 未启用</span>
-
-          </p>
+    <header class="mx-[40px] my-[20px]">
+      <div class="flex justify-between  mb-btnybase items-center">
+        <div class="flex">
+          <div class="mr-btnrbase w-[50px] h-[50px]">
+            <img
+              class="mr-btnrbase"
+              [src]="icon"
+              alt="icon"
+              width="50px"
+              height="50px"
+            />
+          </div>
+          <div class="flex flex-col justify-around">
+            <p class="text-[14px] font-medium">{{ title }}</p>
+            <p>
+              <span
+                class="mr-[8px] font-medium text-[#0A89FF] bg-[#0A89FF1A] px-[4px] py-[2px] leading-[20px] rounded"
+                *ngIf="enable"
+                >已启用</span
+              >
+              <span
+                class="mr-[8px] font-medium text-[#bbbbbb] bg-[#bbbbbb1A] px-[4px] py-[2px] leading-[20px] rounded"
+                *ngIf="!enable"
+              >
+                未启用</span
+              >
+            </p>
+          </div>
+        </div>
+        <div>
+          <button
+            *ngIf="!enable"
+            class="ml-btnybase ant-btn-primary"
+            eo-ng-button
+            (click)="enablePlugin()"
+          >
+            启用
+          </button>
+          <button
+            *ngIf="enable"
+            class="ml-btnybase"
+            eo-ng-button
+            (click)="disablePluginModal(false)"
+          >
+            停用
+          </button>
+          <button
+            *ngIf="!enable && uninstall"
+            class="ml-btnybase"
+            eo-ng-button
+            (click)="disablePluginModal(true)"
+          >
+            卸载
+          </button>
         </div>
       </div>
-      <div>
-      <button
-        *ngIf="!enable"
-        class="ml-btnybase ant-btn-primary"
-        eo-ng-button
-        (click)="enablePlugin()"
-      >
-        启用
-      </button>
-      <button
-        *ngIf="enable"
-        class="ml-btnybase"
-        eo-ng-button
-        (click)="disablePluginModal(false)"
-      >
-        停用
-      </button>
-      <button
-      *ngIf="!enable && uninstall"
-        class="ml-btnybase"
-        eo-ng-button
-        (click)="disablePluginModal(true)"
-      >
-        卸载
-      </button>
-      </div>
-    </div>
-    <p>{{resume}}</p>
-
-  </header>
-  <section class="flex-1 p-[40px] markdown-block overflow-auto">
-      <eo-ng-empty *ngIf="showEmpty" nzMainTitle="暂无数据" nzInputImage="simple"></eo-ng-empty>
-    <markdown  clipboard class="markdown-body" *ngIf="!showEmpty" [src]="getMd()" [srcRelativeLink]="true"  (load)="loadMd()" (error)="onError($event)"></markdown>
-  </section>
+      <p>{{ resume }}</p>
+    </header>
+    <section class="flex-1 p-[40px] markdown-block overflow-auto">
+      <eo-ng-empty
+        *ngIf="showEmpty"
+        nzMainTitle="暂无数据"
+        nzInputImage="simple"
+      ></eo-ng-empty>
+      <markdown
+        clipboard
+        class="markdown-body"
+        *ngIf="!showEmpty"
+        [src]="getMd()"
+        [srcRelativeLink]="true"
+        (load)="loadMd()"
+        (error)="onError($event)"
+      ></markdown>
+    </section>
   `,
-  styleUrls: [
-    './message.component.scss'
-  ]
+  styleUrls: ['./message.component.scss']
 })
 export class PluginMessageComponent implements OnInit {
-  title:string = ''
-  resume:string = ''
-  icon:string = '' || './assets/default-plugin-icon.svg'
-  enable:boolean = false
-  uninstall:boolean = false
-  pluginId:string = ''
-  modalRef:NzModalRef|undefined
-  mdFileName:string = ''
-  showEmpty:boolean = false
-  constructor (private message:EoNgMessageService,
-    private modalService:EoNgFeedbackModalService,
-    private api:ApiService, private baseInfo:BaseInfoService,
+  title: string = ''
+  resume: string = ''
+  icon: string = '' || './assets/default-plugin-icon.svg'
+  enable: boolean = false
+  uninstall: boolean = false
+  pluginId: string = ''
+  modalRef: NzModalRef | undefined
+  mdFileName: string = ''
+  showEmpty: boolean = false
+  constructor(
+    private message: EoNgMessageService,
+    private modalService: EoNgFeedbackModalService,
+    private api: ApiService,
+    private baseInfo: BaseInfoService,
     private appConfigService: EoNgNavigationService,
     private markdownService: MarkdownService,
-    private router:Router,
-    @Inject(API_URL) public urlPrefix:string) {
+    private router: Router,
+    @Inject(API_URL) public urlPrefix: string
+  ) {
     this.appConfigService.reqFlashBreadcrumb([
       { title: '企业插件', routerLink: ['/', 'plugin', 'group', 'list', ''] },
       { title: '插件详情' }
     ])
   }
 
-  ngOnInit (): void {
+  ngOnInit(): void {
     this.pluginId = this.baseInfo.allParamsInfo.pluginId
     this.mdFileName = this.baseInfo.allParamsInfo.mdFileName
     this.getPluginDetail()
     this.markdownService.renderer.link = (href, title, text) => {
       let html = ''
-      if (href && /^(?![http])[.]*/.test(href!) && /^(?![#])[.]*/.test(href!) && href.includes('.md')) {
+      if (
+        href &&
+        /^(?![http])[.]*/.test(href!) &&
+        /^(?![#])[.]*/.test(href!) &&
+        href.includes('.md')
+      ) {
         html = `<a href="plugin/message/${this.pluginId}/${href}">${text}</a>`
-      } else if (href && /^(?![http])[.]*/.test(href!) && /^(?![#])[.]*/.test(href!)) {
+      } else if (
+        href &&
+        /^(?![http])[.]*/.test(href!) &&
+        /^(?![#])[.]*/.test(href!)
+      ) {
         html = `<a href="plugin/message/${this.pluginId}/${href}">${text}</a>`
       } else {
-        html = '<a  role="link"  tabindex="0" target="_blank" rel="nofollow noopener noreferrer" href="' + href + '">' + text + '</a>'
+        html =
+          '<a  role="link"  tabindex="0" target="_blank" rel="nofollow noopener noreferrer" href="' +
+          href +
+          '">' +
+          text +
+          '</a>'
       }
 
       return html
@@ -122,35 +168,51 @@ export class PluginMessageComponent implements OnInit {
     }
   }
 
-  getPluginDetail () {
-    this.api.get('system/plugin/info', { id: this.pluginId })
-      .subscribe((resp:{code:number, data:{plugin:PluginMessage}, msg:string}) => {
-        if (resp.code === 0) {
-          this.title = resp.data.plugin.cname
-          this.resume = resp.data.plugin.resume
-          this.icon = resp.data.plugin.icon ? `${this.urlPrefix}plugin/icon/${this.pluginId}/${resp.data.plugin.icon}` : './assets/default-plugin-icon.svg'
-          this.enable = resp.data.plugin.enable
-          this.uninstall = resp.data.plugin.uninstall
+  getPluginDetail() {
+    this.api
+      .get('system/plugin/info', { id: this.pluginId })
+      .subscribe(
+        (resp: {
+          code: number
+          data: { plugin: PluginMessage }
+          msg: string
+        }) => {
+          if (resp.code === 0) {
+            this.title = resp.data.plugin.cname
+            this.resume = resp.data.plugin.resume
+            this.icon = resp.data.plugin.icon
+              ? `${this.urlPrefix}plugin/icon/${this.pluginId}/${resp.data.plugin.icon}`
+              : './assets/default-plugin-icon.svg'
+            this.enable = resp.data.plugin.enable
+            this.uninstall = resp.data.plugin.uninstall
+          }
         }
-      })
+      )
   }
 
-  getMd () {
-    // return './assets/README.md' // 本地调试用
-    return `../../plugin/md/${this.pluginId}/${this.mdFileName || ''}`
+  getMd() {
+    return './assets/README.md' // 本地调试用
+    // return `../../plugin/md/${this.pluginId}/${this.mdFileName || ''}`
   }
 
-  loadMd () {
+  loadMd() {
     this.showEmpty = false
   }
 
-  onError (value:any) {
+  onError(value: any) {
     console.error('解析md文档出现问题', value)
     this.showEmpty = true
   }
 
-  enablePlugin () {
-    const params:{name:string, server:string, headerList:Array<PluginInstallConfigData>, queryList:Array<PluginInstallConfigData>, initializeList:Array<PluginInstallConfigData>, showServer:boolean } = {
+  enablePlugin() {
+    const params: {
+      name: string
+      server: string
+      headerList: Array<PluginInstallConfigData>
+      queryList: Array<PluginInstallConfigData>
+      initializeList: Array<PluginInstallConfigData>
+      showServer: boolean
+    } = {
       name: '',
       server: '',
       headerList: [],
@@ -158,66 +220,92 @@ export class PluginMessageComponent implements OnInit {
       initializeList: [],
       showServer: false
     }
-    this.api.get('system/plugin/enable', { id: this.pluginId }).subscribe((resp:{code:number, data:PluginInstallData, msg:string}) => {
-      if (resp.code === 30001) {
-        this.message.success(resp.msg || '启用插件成功')
-        const subscription = this.appConfigService.getMenuList().subscribe(() => {
-          subscription.unsubscribe()
-        })
-        this.getPluginDetail()
-      } else if (resp.code === 0) {
-        params.name = resp.data.module.name
-        params.server = resp.data.module.server
-        params.headerList = resp.data.module.header.map((header:PluginInstallConfigData) => {
-          header.placeholder = header.placeholder || '请输入'
-          return header
-        })
-        params.queryList = resp.data.module.query.map((query:PluginInstallConfigData) => {
-          query.placeholder = query.placeholder || '请输入'
-          return query
-        })
-        params.initializeList = resp.data.module.initialize.map((initItem:PluginInstallConfigData) => {
-          initItem.placeholder = initItem.placeholder || '请输入'
-          return initItem
-        })
-        params.showServer = resp.data.render.internet
-
-        if (params.showServer || params.headerList.length || params.queryList.length || params.initializeList.length) {
-          this.modalService.create({
-            nzTitle: '启用',
-            nzWidth: MODAL_NORMAL_SIZE,
-            nzContent: PluginConfigComponent,
-            nzComponentParams: {
-              refreshPage: this.getPluginDetail,
-              pluginId: this.pluginId
-
-            },
-            nzOkText: '确定',
-            nzCancelText: '取消',
-            nzOnOk: (component:PluginConfigComponent) => {
-              component.enablePlugin()
-              return false
-            }
-          })
-        } else {
-          this.api.post('system/plugin/enable', { name: params.name }, { id: this.pluginId }).subscribe((resp:EmptyHttpResponse) => {
-            if (resp.code === 0) {
-              this.message.success(resp.msg || '启用插件成功')
-              const subscription = this.appConfigService.getMenuList().subscribe(() => {
+    this.api
+      .get('system/plugin/enable', { id: this.pluginId })
+      .subscribe(
+        (resp: { code: number; data: PluginInstallData; msg: string }) => {
+          if (resp.code === 30001) {
+            this.message.success(resp.msg || '启用插件成功')
+            const subscription = this.appConfigService
+              .getMenuList()
+              .subscribe(() => {
                 subscription.unsubscribe()
               })
-              this.getPluginDetail()
+            this.getPluginDetail()
+          } else if (resp.code === 0) {
+            params.name = resp.data.module.name
+            params.server = resp.data.module.server
+            params.headerList = resp.data.module.header.map(
+              (header: PluginInstallConfigData) => {
+                header.placeholder = header.placeholder || '请输入'
+                return header
+              }
+            )
+            params.queryList = resp.data.module.query.map(
+              (query: PluginInstallConfigData) => {
+                query.placeholder = query.placeholder || '请输入'
+                return query
+              }
+            )
+            params.initializeList = resp.data.module.initialize.map(
+              (initItem: PluginInstallConfigData) => {
+                initItem.placeholder = initItem.placeholder || '请输入'
+                return initItem
+              }
+            )
+            params.showServer = resp.data.render.internet
+
+            if (
+              params.showServer ||
+              params.headerList.length ||
+              params.queryList.length ||
+              params.initializeList.length
+            ) {
+              this.modalService.create({
+                nzTitle: '启用',
+                nzWidth: MODAL_NORMAL_SIZE,
+                nzContent: PluginConfigComponent,
+                nzComponentParams: {
+                  refreshPage: this.getPluginDetail,
+                  pluginId: this.pluginId
+                },
+                nzOkText: '确定',
+                nzCancelText: '取消',
+                nzOnOk: (component: PluginConfigComponent) => {
+                  component.enablePlugin()
+                  return false
+                }
+              })
+            } else {
+              this.api
+                .post(
+                  'system/plugin/enable',
+                  { name: params.name },
+                  { id: this.pluginId }
+                )
+                .subscribe((resp: EmptyHttpResponse) => {
+                  if (resp.code === 0) {
+                    this.message.success(resp.msg || '启用插件成功')
+                    const subscription = this.appConfigService
+                      .getMenuList()
+                      .subscribe(() => {
+                        subscription.unsubscribe()
+                      })
+                    this.getPluginDetail()
+                  }
+                })
             }
-          })
+          }
         }
-      }
-    })
+      )
   }
 
-  disablePluginModal (deletePlugin:boolean) {
+  disablePluginModal(deletePlugin: boolean) {
     this.modalRef = this.modalService.create({
       nzTitle: deletePlugin ? '卸载' : '停用',
-      nzContent: `该插件${deletePlugin ? '卸载后将无法找回' : '停用后将无法再使用'}，请确认是否要${deletePlugin ? '卸载' : '停用'}？`,
+      nzContent: `该插件${
+        deletePlugin ? '卸载后将无法找回' : '停用后将无法再使用'
+      }，请确认是否要${deletePlugin ? '卸载' : '停用'}？`,
       nzClosable: true,
       nzClassName: 'delete-modal',
       nzWidth: MODAL_SMALL_SIZE,
@@ -236,30 +324,38 @@ export class PluginMessageComponent implements OnInit {
   }
 
   // 禁用插件
-  disablePlugin () {
-    this.api.post('system/plugin/disable', {}, { id: this.pluginId }).subscribe((resp:EmptyHttpResponse) => {
-      if (resp.code === 0) {
-        this.message.success(resp.msg || '禁用成功')
-        const subscription = this.appConfigService.getMenuList().subscribe(() => {
-          subscription.unsubscribe()
-        })
-        this.getPluginDetail()
-        this.modalRef?.close()
-      }
-    })
+  disablePlugin() {
+    this.api
+      .post('system/plugin/disable', {}, { id: this.pluginId })
+      .subscribe((resp: EmptyHttpResponse) => {
+        if (resp.code === 0) {
+          this.message.success(resp.msg || '禁用成功')
+          const subscription = this.appConfigService
+            .getMenuList()
+            .subscribe(() => {
+              subscription.unsubscribe()
+            })
+          this.getPluginDetail()
+          this.modalRef?.close()
+        }
+      })
   }
 
   // 卸载插件
-  deletePlugin () {
-    this.api.post('system/plugin/uninstall', {}, { id: this.pluginId }).subscribe((resp:EmptyHttpResponse) => {
-      if (resp.code === 0) {
-        this.message.success(resp.msg || '卸载成功')
-        const subscription = this.appConfigService.getMenuList().subscribe(() => {
-          subscription.unsubscribe()
-        })
-        this.router.navigate(['/', 'plugin', 'group', 'list'])
-        this.modalRef?.close()
-      }
-    })
+  deletePlugin() {
+    this.api
+      .post('system/plugin/uninstall', {}, { id: this.pluginId })
+      .subscribe((resp: EmptyHttpResponse) => {
+        if (resp.code === 0) {
+          this.message.success(resp.msg || '卸载成功')
+          const subscription = this.appConfigService
+            .getMenuList()
+            .subscribe(() => {
+              subscription.unsubscribe()
+            })
+          this.router.navigate(['/', 'plugin', 'group', 'list'])
+          this.modalRef?.close()
+        }
+      })
   }
 }
