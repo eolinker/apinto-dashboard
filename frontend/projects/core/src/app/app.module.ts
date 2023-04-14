05c125e22c596f5d577450f5974a805bcb7f1e37
maggieyyy <61950669+maggieyyy@users.noreply.github.com>
2023-04-14 10:03:39 +0800

    frontend-fix:安装插件失败后保留弹窗

--- frontend/projects/core/src/app/app.module.ts
+++ frontend/projects/core/src/app/app.module.ts
@@ -41,7 +41,6 @@ const ngZorroConfig: NzConfig = {
 export function markedOptionsFactory (): MarkedOptions {
   const renderer = new MarkedRenderer()
   const linkRenderer = renderer.link
-  console.log('markdown')
   renderer.link = (href, title, text) => {
     let html = linkRenderer.call(renderer, href, title, text)
     html = html.replace(/^<a /, '<a role="link"  tabindex="0" target="_blank" rel="nofollow noopener noreferrer" ')
