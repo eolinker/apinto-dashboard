66b68dee356d0e5697ecc6655fbcc32fe75c3990
maggieyyy <61950669+maggieyyy@users.noreply.github.com>
2023-04-23 18:02:22 +0800

    frontend-feat:智能插件

---
+++ frontend/projects/core/src/app/component/formily2-react/IntelligentPluginEditComponent.tsx
@@ -0,0 +1,218 @@
+// eslint-disable-next-line no-use-before-define
+import * as React from 'react'
+
+import {
+  FormItem,
+  Space,
+  ArrayItems,
+  DatePicker,
+  Editable,
+  FormButtonGroup,
+  Input,
+  Radio,
+  Select,
+  Submit,
+  Cascader,
+  Form,
+  FormGrid,
+  FormLayout,
+  Upload,
+  ArrayCollapse,
+  ArrayTable,
+  ArrayTabs,
+  Checkbox,
+  FormCollapse,
+  FormDialog,
+  FormDrawer,
+  FormStep,
+  FormTab,
+  NumberPicker,
+  Password,
+  PreviewText,
+  Reset,
+  SelectTable,
+  Switch,
+  TimePicker,
+  Transfer,
+  TreeSelect,
+  ArrayCards
+} from '@formily/antd'
+import { createForm } from '@formily/core'
+import {
+  FormProvider,
+  RecursionField,
+  createSchemaField,
+  observer,
+  useField,
+  useForm
+} from '@formily/react'
+import { CustomCodeboxComponent } from './component/codebox/CustomCodeboxComponent'
+import { CustomEnvVariableComponent } from './component/editable-env-table/CustomEnvVariableComponent'
+import { isEqual } from 'lodash-es'
+
+const DynamicRender = observer(() => {
+  const field = useField()
+  const form = useForm()
+  const [schema, setSchema] = React.useState({})
+
+  React.useEffect(() => {
+    form.clearFormGraph(`${field.address}.*`)
+    setSchema(DYNAMIC_INJECT_SCHEMA[form.values.driver])
+    console.log(
+      DYNAMIC_INJECT_SCHEMA,
+      form.values.driver,
+      DYNAMIC_INJECT_SCHEMA[form.values.driver]
+    )
+  }, [form.values.driver])
+
+  return (
+    <RecursionField
+      basePath={field.address}
+      schema={schema}
+      onlyRenderProperties
+    />
+  )
+})
+
+const SchemaField = createSchemaField({
+  components: {
+    ArrayCards,
+    ArrayCollapse,
+    ArrayItems,
+    ArrayTable,
+    ArrayTabs,
+    Cascader,
+    Checkbox,
+    DatePicker,
+    Editable,
+    Form,
+    FormButtonGroup,
+    FormCollapse,
+    FormDialog,
+    FormDrawer,
+    FormGrid,
+    FormItem,
+    FormLayout,
+    FormStep,
+    FormTab,
+    Input,
+    NumberPicker,
+    Password,
+    PreviewText,
+    Radio,
+    Reset,
+    Select,
+    SelectTable,
+    Space,
+    Submit,
+    Switch,
+    TimePicker,
+    Transfer,
+    TreeSelect,
+    Upload,
+    DynamicRender,
+    CustomCodeboxComponent,
+    CustomEnvVariableComponent
+  }
+})
+
+let DYNAMIC_INJECT_SCHEMA: any
+
+export const IntelligentPluginEditComponent = React.forwardRef(
+  (props: { [k: string]: any }, ref) => {
+    const { schema, initFormValue, driverSelectOptions, demo, demoSchema } =
+      props
+    React.useImperativeHandle(ref, () => ({ form, submitRef }))
+
+    const submitRef = React.createRef()
+    DYNAMIC_INJECT_SCHEMA = schema
+    console.log(demo, demoSchema)
+
+    const form = createForm()
+
+    form.setInitialValues(initFormValue)
+    const pluginEditSchema = {
+      type: 'object',
+      properties: {
+        id: {
+          type: 'string',
+          title: 'ID',
+          required: true,
+          'x-decorator': 'FormItem',
+          'x-decorator-props': {
+            labelCol: 6,
+            wrapperCol: 10
+          },
+          'x-component': 'Input',
+          'x-component-props': {
+            placeholder: 'ID'
+          }
+        },
+        name: {
+          type: 'string',
+          title: '名称',
+          required: true,
+          'x-decorator': 'FormItem',
+          'x-decorator-props': {
+            labelCol: 6,
+            wrapperCol: 10
+          },
+          'x-component': 'Input',
+          'x-component-props': {
+            placeholder: '名称'
+          }
+        },
+        driver: {
+          type: 'string',
+          title: 'Driver',
+          required: true,
+          'x-decorator': 'FormItem',
+          'x-decorator-props': {
+            labelCol: 6,
+            wrapperCol: 10
+          },
+          'x-component': 'Select',
+          'x-display': driverSelectOptions.length > 1 ? 'visible' : 'hidden',
+          enum: [...driverSelectOptions]
+        },
+        desc: {
+          type: 'string',
+          required: true,
+          title: '描述',
+          'x-decorator': 'FormItem',
+          'x-decorator-props': {
+            labelCol: 6,
+            wrapperCol: 10
+          },
+          'x-component': 'Input.TextArea',
+          'x-component-props': {
+            placeholder: 'ID'
+          }
+        },
+        container: {
+          type: 'void',
+          'x-component': 'DynamicRender',
+          'x-component-props': {
+            schema: schema
+          }
+        }
+      }
+    }
+
+    const submit = (value: any) => {
+      console.log(value)
+    }
+    return (
+      <FormProvider form={form} layout="vertical">
+        <SchemaField schema={demo ? demoSchema : pluginEditSchema} />
+        {demo && demoSchema && (
+          <FormButtonGroup>
+            <Submit ref={submitRef} onSubmit={submit}>
+              提交
+            </Submit>
+          </FormButtonGroup>
+        )}
+      </FormProvider>
+    )
+  }
+)
--- frontend/projects/core/src/app/component/table-status-td/table-status-td.component.ts
+++ frontend/projects/core/src/app/component/table-status-td/table-status-td.component.ts
@@ -5,9 +5,13 @@ import { Component, Input } from '@angular/core'
   template: `
     <ng-container [ngSwitch]="status">
     <span *ngSwitchCase="'TOUPDATE'" class="blue-bold">待更新</span>
+    <span *ngSwitchCase="'待更新'" class="blue-bold">待更新</span>
     <span *ngSwitchCase="'GOONLINE'" class="green-bold">已上线</span>
+    <span *ngSwitchCase="'已上线'" class="green-bold">已上线</span>
     <span *ngSwitchCase="'OFFLINE'" class="grey-bold">已下线</span>
+    <span *ngSwitchCase="'已下线'" class="grey-bold">已下线</span>
     <span *ngSwitchCase="'NOTGOONLINE'" class="grey-bold">未上线</span>
+    <span *ngSwitchCase="'未上线'" class="grey-bold">未上线</span>
     <span *ngSwitchCase="'TODELETE'" class="orange-bold">待删除</span>
     </ng-container>
   `,
