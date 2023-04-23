ea5b08aba2002e1a8ec1cb95bd8ba39ba61dbca9
maggieyyy <61950669+maggieyyy@users.noreply.github.com>
2023-04-23 18:02:22 +0800

    frontend-feat:智能插件

--- frontend/projects/core/src/app/layout/iframe-page/iframe-page.component.ts
+++ frontend/projects/core/src/app/layout/iframe-page/iframe-page.component.ts
@@ -1,15 +1,47 @@
-import { Component } from '@angular/core'
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core'
+import { IframeHttpService } from '../../service/iframe-http.service'
 
 @Component({
   selector: 'eo-ng-iframe-page',
   template: `
-    <p>
-      iframe-page works!
-    </p>
+    <iframe [src] ="iframeSrc | safe:'resourceUrl'" #iframe (load)="onLoad($event)" scrolling="yes">
+    <p>Your browser does not support iframe.</p>
+    </iframe>
   `,
   styles: [
+    `
+    iframe{
+      width:100%;
+      height:100%;
+      border:none;
+    }`
   ]
 })
-export class IframePageComponent {
+export class IframePageComponent implements OnInit {
+  @ViewChild('iframe') iframe: ElementRef|undefined;
+  @Input() path:string ='monitor-alarm'
+  iframeSrc:string = ''
+  constructor (private iframeService:IframeHttpService) {}
+  ngOnInit (): void {
+    // console.log(window.frames.ifr1.window)
+    // console.dir(document.getElementById('ifr1')?.contentWindow)
+    this.getIframeSrc()
+  }
 
+  // 打开的iframe可能需要传入header
+  getIframeSrc () {
+    this.iframeService.openIframe(this.path, { headers: { test: 'test' } }).subscribe((blob) => {
+      this.iframeSrc = blob
+    })
+  }
+
+  // ngAfterViewInit () {
+  //   const doc = this.iframe!.nativeElement.contentDocument || this.iframe!.nativeElement.contentWindow
+  // }
+
+  onLoad (iframe:any) {
+    console.log('---------')
+    const doc = iframe!.nativeElement?.contentDocument || iframe!.nativeElement?.contentWindow
+    console.log('---------', iframe, doc)
+  }
 }
