b5131e755996054833477b82777615388eeac444
huangmengzhu <hmzzrcs@163.com>
2023-04-24 19:28:03 +0800

    登录记录

--- modules/user/user-entry/user_info.go
+++ modules/user/user-entry/user_info.go
@@ -5,7 +5,7 @@ import "time"
 type UserInfo struct {
 	Id            int        `gorm:"type:int(11);size:11;not null;auto_increment;primary_key;column:id;comment:主键ID"`                                //用户ID
 	Sex           int        `gorm:"type:int(11);size:11;not null;default:0;column:sex;comment:0未知1男2女"`                                             //0未知 1男2女
-	UserName      string     `gorm:"size:36;not null;index:username;column:username;comment:账号名"`                                                    //账号
+	UserName      string     `gorm:"size:36;not null;index:username;dbUniqueIndex:username;column:username;comment:账号名"`                             //账号
 	NoticeUserId  string     `gorm:"size:36;not null;column:notice;comment:通知用户ID"`                                                                  //通知用户ID
 	NickName      string     `gorm:"size:36;not null;column:nickname;comment:昵称"`                                                                    //用户昵称
 	Email         string     `gorm:"size:255;default:null;column:email;comment:邮箱地址"`                                                                //邮箱
