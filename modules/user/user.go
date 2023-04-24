27ef3165ae89823436d0de56c41de00c6969e225
chenjiekun <chenjiekun@sina.cn>
2023-04-24 16:27:04 +0800

    用户服务新增两个接口，通过用户名以及用户名列表获取用户信息

--- modules/user/user.go
+++ modules/user/user.go
@@ -8,4 +8,6 @@ import (
 type IUserInfoService interface {
 	GetUserInfo(ctx context.Context, userId int) (*user_model.UserInfo, error)
 	GetUserInfoMaps(ctx context.Context, userId ...int) (map[int]*user_model.UserInfo, error)
+	GetUserInfoByName(ctx context.Context, userName string) (*user_model.UserInfo, error)
+	GetUserInfoByNames(ctx context.Context, userNames ...string) (map[string]*user_model.UserInfo, error)
 }
