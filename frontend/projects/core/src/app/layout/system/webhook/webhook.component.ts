16c1d91fd516f9ce1b2330f6df21473a26f19892
maggieyyy <61950669+maggieyyy@users.noreply.github.com>
2023-04-25 10:10:38 +0800

    frontend-fix:监控改造

---
+++ frontend/projects/core/src/app/layout/system/webhook/webhook.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'eo-ng-system-webhook',
+  template: `
+    <router-outlet></router-outlet>
+  `,
+  styles: [
+  ]
+})
+export class SystemWebhookComponent {
+
+}
