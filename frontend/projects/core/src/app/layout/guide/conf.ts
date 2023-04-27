9d897bb50f086ac69ac26a5c7e8ea2ea10b0bbf8
maggieyyy <61950669+maggieyyy@users.noreply.github.com>
2023-04-27 20:41:19 +0800

    frontend-fix:修改快速入门

--- frontend/projects/core/src/app/layout/guide/conf.ts
+++ frontend/projects/core/src/app/layout/guide/conf.ts
@@ -1,10 +1,11 @@
-export type StepItem = {name:'cluster'|'upstream'|'api'|'publishApi',
+export type StepItem = {name:'cluster'|'upstream'|'api'|'publishApi'|'final',
 title:string,
 desc:Array<string>,
-status:'undo'|'doing'|'done'
+status?:'undo'|'doing'|'done'
 img?:string
-toDoUrl:string
-doneUrl:string}
+toDoUrl?:string
+doneUrl?:string,
+color?:string}
 
 export type TutorialItem = {
       title:string,
@@ -17,35 +18,45 @@ export type TutorialItem = {
 export const GuideStepList:Array<StepItem> = [
   {
     name: 'cluster',
-    title: '创建集群',
+    title: '创建网关集群',
     desc: ['创建 Apinto 网关集群，集群用于承载网络流量'],
     status: 'undo',
     toDoUrl: 'deploy/cluster/create',
-    doneUrl: 'deploy/cluster'
+    doneUrl: 'deploy/cluster',
+    color: '#ee6760'
   },
   {
     name: 'upstream',
-    title: '添加上游服务',
+    title: '添加转发目标 （上游）',
     desc: ['添加上游服务器或动态服务发现，接收网关节点转发的流量'],
     status: 'undo',
     toDoUrl: 'upstream/upstream/create',
-    doneUrl: 'upstream/upstream'
+    doneUrl: 'upstream/upstream',
+    color: '#f9a429'
   },
   {
     name: 'api',
-    title: '添加 API',
+    title: '添加转发的 API',
     desc: ['添加需要网关转发的 API'],
     status: 'undo',
     toDoUrl: 'router/api/create',
-    doneUrl: 'router/api/group/list'
+    doneUrl: 'router/api/group/list',
+    color: '#71d24d'
   },
   {
     name: 'publishApi',
-    title: '发布 API',
+    title: '创建应用',
     desc: ['发布之后就可以通过 Apinto 安全高效地访问API啦！'],
     status: 'undo',
-    toDoUrl: 'router/api/group/list',
-    doneUrl: 'router/api/group/list'
+    toDoUrl: 'application/list',
+    doneUrl: 'application/list',
+    color: '#5884ff'
+  },
+  {
+    name: 'final',
+    title: '发起调用',
+    desc: ['发布之后就可以通过 Apinto 安全高效地访问API啦！'],
+    color: '#915bf9'
   }
 ]
 
