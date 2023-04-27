c116cba664c6184e21f27999f350fcdb3f8bd35b
Liujian <824010343@qq.com>
2023-04-27 21:59:30 +0800

    用户接口完成

--- db_migrator/sql/v2.3.1_ddl_init.sql
+++ db_migrator/sql/v2.3.1_ddl_init.sql
@@ -1,17 +1,14 @@
-create table if not exists `user`
-(
-    id         int(10) auto_increment comment '主键id'
-        primary key,
-    sex        int default 0 not null comment '性别，0未知，1男2女',
-    username   varchar(36)   not null comment '用户名',
-    notice     varchar(36)   null comment '通知key',
-    nickname   varchar(255)  null comment '昵称',
-    email      varchar(255)  null comment '邮箱',
-    phone      varchar(20)   null comment '手机号',
-    avatar     varchar(255)  null comment '头像',
-    login_time timestamp     null comment '最后登录时间',
-    constraint user_pk2
-        unique (username)
-)
-    comment '用户信息表';
-
+CREATE TABLE IF NOT EXISTS `user` (
+    `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+    `sex` int(11) NOT NULL DEFAULT '0' COMMENT '性别，0未知，1男2女',
+    `username` varchar(36) NOT NULL COMMENT '用户名',
+    `password` varchar(32) NOT NULL COMMENT '密码',
+    `notice` varchar(36) DEFAULT NULL COMMENT '通知key',
+    `nickname` varchar(255) DEFAULT NULL COMMENT '昵称',
+    `email` varchar(255) DEFAULT NULL COMMENT '邮箱',
+    `phone` varchar(20) DEFAULT NULL COMMENT '手机号',
+    `avatar` varchar(255) DEFAULT NULL COMMENT '头像',
+    `login_time` timestamp NULL DEFAULT NULL COMMENT '最后登录时间',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `user_pk2` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户信息表';
