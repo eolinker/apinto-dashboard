f3b9123a6bebe93bb66fb32d67b8fc07f2773038
Liujian <824010343@qq.com>
2023-05-04 18:31:40 +0800

    临时提交

--- modules/dynamic/dynamic.go
+++ modules/dynamic/dynamic.go
@@ -11,6 +11,7 @@ import (
 type IDynamicService interface {
 	Info(ctx context.Context, namespaceId int, profession string, name string) (*v2.WorkerInfo[dynamic_model.DynamicBasicInfo], error)
 	List(ctx context.Context, namespaceId int, profession string, columns []string, drivers []string, keyword string, page int, pageSize int) ([]map[string]string, int, error)
+	GetBySkill(ctx context.Context, namespaceId int, skill string) ([]*dynamic_model.DynamicBasicInfo, error)
 
 	Online(ctx context.Context, namespaceId int, profession string, name string, names []string, updater int) ([]string, []string, error)
 	Offline(ctx context.Context, namespaceId int, profession string, name string, names []string, updater int) ([]string, []string, error)
@@ -18,7 +19,7 @@ type IDynamicService interface {
 	ClusterStatuses(ctx context.Context, namespaceId int, profession string, names []string, drivers []string, keyword string, page int, pageSize int) (map[string]map[string]string, error)
 	ClusterStatus(ctx context.Context, namespaceId int, profession string, name string) (*dynamic_model.DynamicBasicInfo, []*dynamic_model.DynamicCluster, error)
 
-	Create(ctx context.Context, namespaceId int, module string, title string, name string, driver string, description string, body string, updater int) error
-	Save(ctx context.Context, namespaceId int, module string, title string, name string, description string, body string, updater int) error
-	Delete(ctx context.Context, namespaceId int, module string, name string) error
+	Create(ctx context.Context, namespaceId int, profession string, skill string, title string, name string, driver string, description string, body string, updater int) error
+	Save(ctx context.Context, namespaceId int, profession string, title string, name string, description string, body string, updater int) error
+	Delete(ctx context.Context, namespaceId int, profession string, name string) error
 }
