6eebd91b2079f5ef7a2a63bfc7d2653256a6e3ff
Liujian <824010343@qq.com>
2023-05-10 21:03:38 +0800

    修复应用发布失败问题

---
+++ client/v2/app.go
@@ -0,0 +1,27 @@
+package v2
+
+func dealAPPAppend(data map[string]interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+	for key, value := range data {
+		if key == "additional" || key == "auths" {
+			vs, ok := value.([]interface{})
+			if !ok {
+				continue
+			}
+			params := make([]interface{}, 0, len(vs))
+			for _, v := range vs {
+				val, ok := v.(map[string]interface{})
+				if !ok {
+					continue
+				}
+				vv, ok := val["config"]
+				if ok {
+					params = append(params, vv)
+				}
+			}
+			result[key] = params
+			continue
+		}
+	}
+	return result
+}
