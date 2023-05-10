d8f8d0c565715e9e7e0da35f0ec738919fad4196
chenjiekun <chenjiekun@sina.cn>
2023-05-10 15:25:58 +0800

    修复api 插件模板bug

--- modules/base/frontend-model/frontend.go
+++ modules/base/frontend-model/frontend.go
@@ -5,8 +5,8 @@ type RouterName string
 var (
 	RouterNameClusterVariable RouterName = "deploy/cluster/content"          //cluster_name
 	RouterNameDiscoveryOnline RouterName = "upstream/serv-discovery/content" //discovery_name
-	RouterNameServiceOnline   RouterName = "upstream/upstream/content/publish"
-	RouterNameTemplateOnline  RouterName = "router/plugin/content"
+	RouterNameServiceOnline   RouterName = "template/upstream"
+	RouterNameTemplateOnline  RouterName = "router/plugin-template/content"
 	RouterNameClusterPlugin   RouterName = "deploy/cluster/content/plugin"
 )
 
