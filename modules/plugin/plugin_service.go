313e1b1d04b55ba179686bdb97f5a82198419375
chenjiekun <chenjiekun@sina.cn>
2023-05-17 18:12:56 +0800

    修复获取插件schema的bug

--- modules/plugin/plugin_service.go
+++ modules/plugin/plugin_service.go
@@ -7,11 +7,13 @@ import (
 
 type IPluginService interface {
 	GetList(ctx context.Context, namespaceId int) ([]*plugin_model.Plugin, error)
+	GetEnumList(ctx context.Context, namespaceId int) ([]*plugin_model.PluginEnum, error)
 	Create(ctx context.Context, namespaceId, operator int, input *plugin_model.PluginInput) error
 	Update(ctx context.Context, namespaceId, operator int, input *plugin_model.PluginInput) error
 	Delete(ctx context.Context, namespaceId, operator int, name string) error
 	GetByName(ctx context.Context, namespaceId int, name string) (*plugin_model.Plugin, error)
-	GetBasicInfoList(ctx context.Context, namespaceId int) ([]*plugin_model.PluginBasic, error)  //只获取插件基本信息
-	InsertBuilt(ctx context.Context, namespaceId int, plugins []*plugin_model.PluginBuilt) error //新增内置插件
+	GetBasicInfoList(ctx context.Context, namespaceId int) ([]*plugin_model.PluginBasic, error)    //只获取插件基本信息
+	InsertBuiltin(ctx context.Context, namespaceId int, plugins []*plugin_model.PluginBuilt) error //新增内置插件
 	Sort(ctx context.Context, namespaceId, operator int, names []string) error
+	GetExtendersCache(ctx context.Context, namespaceId int) ([]*plugin_model.ExtenderInfo, error)
 }
