c1d8cfc254b7e47eb68104a609523e0d30617b1c
huangmengzhu <hmzzrcs@163.com>
2023-05-19 15:31:55 +0800

    调整db结构

--- app/apserver/store-mysql.go
+++ app/apserver/store-mysql.go
@@ -3,11 +3,11 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"github.com/eolinker/apinto-dashboard/db_migrator"
 	_ "github.com/eolinker/apinto-dashboard/initialize"
 	"github.com/eolinker/apinto-dashboard/store"
+	"github.com/eolinker/apinto-dashboard/store/store_mysql"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -31,44 +31,10 @@ func initDB() {
 	sqlDb.SetMaxOpenConns(200)
 	sqlDb.SetMaxIdleConns(200)
 	db_migrator.InitSql(db)
-	store.InitStoreDB(&myDB{
-		db: db,
-		info: store.DBInfo{
-			Addr: fmt.Sprintf("%s:%s", GetDBIp(), GetDBPort()),
-			User: GetDBUserName(),
-			DB:   GetDbName(),
-		},
-	})
+	store.InitStoreDB(store_mysql.NewMyDB(db, store.DBInfo{
+		Addr: fmt.Sprintf("%s:%s", GetDBIp(), GetDBPort()),
+		User: GetDBUserName(),
+		DB:   GetDbName(),
+	}))
 
-}
-
-var (
-	_ store.IDB = (*myDB)(nil)
-)
-
-type myDB struct {
-	db   *gorm.DB
-	info store.DBInfo
-}
-
-var txContextKey = _TxContextKey{}
-
-type _TxContextKey struct {
-}
-
-func (m *myDB) Info() store.DBInfo {
-	return m.info
-}
-
-func (m *myDB) DB(ctx context.Context) *gorm.DB {
-	if tx, ok := ctx.Value(txContextKey).(*gorm.DB); ok {
-		return tx
-	}
-	return m.db.WithContext(ctx)
-}
-func (m *myDB) IsTxCtx(ctx context.Context) bool {
-	if _, ok := ctx.Value(txContextKey).(*gorm.DB); ok {
-		return ok
-	}
-	return false
 }
