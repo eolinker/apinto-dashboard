480592243b9ea4218059af19d6ab517405080225
chenjiekun <chenjiekun@sina.cn>
2023-05-22 18:45:21 +0800

    解耦model层结构体，修改完成应用列表，新建编辑删除应用，获取应用信息，应用枚举列表接口

--- enum/keyword.go
+++ enum/keyword.go
@@ -1,1 +1,5 @@
 package enum
+
+import "github.com/eolinker/apinto-dashboard/modules/strategy/config"
+
+var Keyword = map[string]struct{}{config.FilterValuesALL: {}, config.FilterApplication: {}, config.FilterApi: {}, config.FilterPath: {}, config.FilterService: {}, config.FilterMethod: {}, config.FilterIP: {}}
