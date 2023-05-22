1129b18e3af0b6e279a54f429d8655aa2be3056d
chenjiekun <chenjiekun@sina.cn>
2023-05-22 18:45:21 +0800

    解耦model层结构体，修改完成应用列表，新建编辑删除应用，获取应用信息，应用枚举列表接口

--- modules/application/application_service.go
+++ modules/application/application_service.go
@@ -4,39 +4,37 @@ import (
 	"context"
 	"github.com/eolinker/apinto-dashboard/client/v1"
 	driverInfo "github.com/eolinker/apinto-dashboard/driver"
-	application_dto2 "github.com/eolinker/apinto-dashboard/modules/application/application-dto"
-	application_model2 "github.com/eolinker/apinto-dashboard/modules/application/application-model"
+	"github.com/eolinker/apinto-dashboard/modules/application/application-dto"
+	"github.com/eolinker/apinto-dashboard/modules/application/application-model"
 )
 
 type IApplicationService interface {
-	CreateApp(ctx context.Context, namespaceId, userId int, input *application_dto2.ApplicationInput) error
-	UpdateApp(ctx context.Context, namespaceId, userId int, input *application_dto2.ApplicationInput) error
+	CreateApp(ctx context.Context, namespaceId, userId int, input *application_dto.ApplicationInput) error
+	UpdateApp(ctx context.Context, namespaceId, userId int, input *application_dto.ApplicationInput) error
 	DelApp(ctx context.Context, namespaceId, userId int, id string) error
-	AppList(ctx context.Context, namespaceId, userId, pageNum, pageSize int, queryName string) ([]*application_model2.Application, int, error)
-	AppListAll(ctx context.Context, namespaceId int) ([]*application_model2.Application, error)
-	AppListFilter(ctx context.Context, namespaceId, pageNum, pageSize int, queryName string) ([]*application_model2.Application, int, error)
-	AppListByUUIDS(ctx context.Context, namespaceId int, uuids []string) ([]*application_model2.Application, error)
-	AppInfoDetails(ctx context.Context, namespaceId int, id string) (*application_model2.Application, error)
-	AppInfo(ctx context.Context, namespaceId int, id string) (*application_model2.Application, error)
+	AppList(ctx context.Context, namespaceId, userId, pageNum, pageSize int, queryName string, clusters []string) ([]*application_model.ApplicationListItem, int, error)
+	AppEnumList(ctx context.Context, namespaceId int) ([]*application_model.ApplicationBasicInfo, error)
+	AppListFilter(ctx context.Context, namespaceId, pageNum, pageSize int, queryName string) ([]*application_model.ApplicationBasicInfo, int, error)
+	AppListByUUIDS(ctx context.Context, namespaceId int, uuids []string) ([]*application_model.ApplicationBasicInfo, error)
+	AppInfoDetails(ctx context.Context, namespaceId int, id string) (*application_model.ApplicationInfo, error)
+	AppInfo(ctx context.Context, namespaceId int, id string) (*application_model.ApplicationBasicInfo, error)
 	Online(ctx context.Context, namespaceId, userId int, id, clusterName string) error
 	Offline(ctx context.Context, namespaceId, userId int, id, clusterName string) error
-	Disable(ctx context.Context, namespaceId, userId int, id, clusterName string, disable bool) error
-	OnlineList(ctx context.Context, namespaceId int, id string) ([]*application_model2.ApplicationOnline, error)
-	GetAppKeys(ctx context.Context, namespaceId int) ([]*application_model2.ApplicationKeys, error)
-	GetAppVersion(ctx context.Context, appId int) (*application_model2.ApplicationVersion, error)
+	OnlineList(ctx context.Context, namespaceId int, id string) ([]*application_model.ApplicationOnline, error)
+	GetAppKeys(ctx context.Context, namespaceId int) ([]*application_model.ApplicationKeys, error)
 	//online.IResetOnlineService
 }
 
 type IApplicationAuthService interface {
-	GetList(ctx context.Context, namespaceId int, appId string) ([]*application_model2.ApplicationAuth, error)
-	Create(ctx context.Context, namespaceId, userId int, appId string, input *application_dto2.ApplicationAuthInput) error
-	Update(ctx context.Context, namespaceId, userId int, appId, uuid string, input *application_dto2.ApplicationAuthInput) error
+	GetList(ctx context.Context, namespaceId int, appId string) ([]*application_model.ApplicationAuth, error)
+	Create(ctx context.Context, namespaceId, userId int, appId string, input *application_dto.ApplicationAuthInput) error
+	Update(ctx context.Context, namespaceId, userId int, appId, uuid string, input *application_dto.ApplicationAuthInput) error
 	Delete(ctx context.Context, namespaceId, userId int, uuid string) error
-	Info(ctx context.Context, namespaceId int, appId, uuid string) (*application_model2.ApplicationAuth, error)
-	Online(ctx context.Context, namespaceId, userId, clusterId, applicationId int) ([]*application_model2.ApplicationAuth, error)
+	Info(ctx context.Context, namespaceId int, appId, uuid string) (*application_model.ApplicationAuth, error)
+	Online(ctx context.Context, namespaceId, userId, clusterId, applicationId int) ([]*application_model.ApplicationAuth, error)
 	Offline(ctx context.Context, clusterId, applicationId int) error
 	IsUpdate(ctx context.Context, clusterId, applicationId int) (bool, error)
-	GetListByApplicationId(ctx context.Context, applicationId int) ([]*application_model2.ApplicationAuth, error)
+	GetListByApplicationId(ctx context.Context, applicationId int) ([]*application_model.ApplicationAuth, error)
 	GetDriversRender() []*driverInfo.DriverInfo
 	GetDriver(driver string) IAuthDriver
 }
