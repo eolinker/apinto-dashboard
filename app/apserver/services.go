4c3bc6896258b1d3e7584845d8946b2be5d2ff6c
chenjiekun <chenjiekun@sina.cn>
2023-05-29 18:01:13 +0800

    实现remote_storage service

--- app/apserver/services.go
+++ app/apserver/services.go
@@ -19,6 +19,7 @@ import (
 	_ "github.com/eolinker/apinto-dashboard/modules/plugin/plugin-service"
 	_ "github.com/eolinker/apinto-dashboard/modules/plugin_template/plugin-template-service"
 	_ "github.com/eolinker/apinto-dashboard/modules/random/random-service"
+	_ "github.com/eolinker/apinto-dashboard/modules/remote_storage/service"
 	_ "github.com/eolinker/apinto-dashboard/modules/strategy/strategy-service"
 	_ "github.com/eolinker/apinto-dashboard/modules/user/user-service"
 	_ "github.com/eolinker/apinto-dashboard/modules/variable/variable-service"
