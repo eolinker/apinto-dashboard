5fec036874adbd5411937858fce1df9996325e31
chenjiekun <chenjiekun@sina.cn>
2023-05-30 12:09:23 +0800

    fix bug

--- modules/remote_storage/entry/remote_key_object.go
+++ modules/remote_storage/entry/remote_key_object.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type RemoteKeyObject struct {
 	Module string      `gorm:"uniqueIndex:module_key;index;not null;size:255;column:module;comment:模块名"`
 	Key    string      `gorm:"uniqueIndex:module_key;not null;size:32;column:key"` //TODO key要多长
-	Object interface{} `gorm:"type:text;serializer:json;column:key"`
+	Object interface{} `gorm:"type:text;serializer:json;column:object"`
 	gorm.Model
 }
 
