d932639156b428c2df2b80795c528949373f2b60
maggieyyy <61950669+maggieyyy@users.noreply.github.com>
2023-06-14 19:04:18 +0800

    frontend-business:日志检索

---
+++ frontend/projects/core/src/app/layout/log-retrieval/log-retrieval.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+import { LogRetrievalRoutingModule } from './log-retrieval-routing.module'
+import { LogRetrievalComponent } from './log-retrieval.component'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { EoNgTreeModule } from 'eo-ng-tree'
+import { EoNgSelectModule } from 'eo-ng-select'
+import { EoNgButtonModule } from 'eo-ng-button'
+import { EoNgCollapseModule } from 'eo-ng-collapse'
+import { EoNgApintoTableModule } from 'projects/eo-ng-apinto-table/src/public-api'
+import { EoNgLogRetrievalTailComponent } from './tail-log/tail-log.component'
+import { EoNgCheckboxModule } from 'eo-ng-checkbox'
+import { EoNgCodeboxModule } from 'eo-ng-codebox'
+
+@NgModule({
+  declarations: [
+    LogRetrievalComponent,
+    EoNgLogRetrievalTailComponent
+  ],
+  imports: [
+    CommonModule,
+    LogRetrievalRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    EoNgCheckboxModule,
+    EoNgTreeModule,
+    EoNgSelectModule,
+    EoNgButtonModule,
+    EoNgCollapseModule,
+    EoNgApintoTableModule,
+    EoNgCodeboxModule
+  ]
+})
+export class LogRetrievalModule { }
