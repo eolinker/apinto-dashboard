441b338be430a096953ef94a16efdd9426ae3d3a
huangmengzhu <hmzzrcs@163.com>
2023-06-19 18:09:09 +0800

    重定向包

--- app/apserver/embed_plugin.go
+++ app/apserver/embed_plugin.go
@@ -1,5 +1,7 @@
 package main
 
 import (
-	_ "github.com/eolinker/apinto-dashboard/initialize"
+	_ "github.com/eolinker/apinto-dashboard/initialize/navigation"
+	_ "github.com/eolinker/apinto-dashboard/initialize/plugin-group"
+	_ "github.com/eolinker/apinto-dashboard/initialize/plugins"
 )
