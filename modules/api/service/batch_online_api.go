01b2d8c7f04527518713cf32e939bf5012c6c1da
huangmengzhu <hmzzrcs@163.com>
2023-06-27 18:09:23 +0800

    重构缓存

--- modules/api/service/batch_online_api.go
+++ modules/api/service/batch_online_api.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/eolinker/apinto-dashboard/cache"
 	apimodel "github.com/eolinker/apinto-dashboard/modules/api/model"
-	"github.com/go-redis/redis/v8"
 	"time"
 )
 
@@ -20,10 +19,10 @@ func formatKey(token string) string {
 	return fmt.Sprintf("batch_online_api_token:%s", token)
 }
 
-func newBatchOnlineTaskCache(client *redis.ClusterClient) IBatchOnlineApiTaskCache {
+func newBatchOnlineTaskCache() IBatchOnlineApiTaskCache {
 
 	return &batchOnlineApiTaskCache{
-		IRedisCache: cache.CreateRedisCache[apimodel.BatchOnlineCheckTask](client, time.Hour*8, formatKey),
+		IRedisCache: cache.CreateRedisCache[apimodel.BatchOnlineCheckTask](time.Hour*8, formatKey),
 	}
 
 }
