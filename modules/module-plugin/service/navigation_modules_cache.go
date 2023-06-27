5af77032034910bbe3ebeebc5ac252e66553d8d5
huangmengzhu <hmzzrcs@163.com>
2023-06-27 18:09:23 +0800

    重构缓存

--- modules/module-plugin/service/navigation_modules_cache.go
+++ modules/module-plugin/service/navigation_modules_cache.go
@@ -4,11 +4,10 @@ import (
 	"github.com/eolinker/apinto-dashboard/cache"
 	module_plugin "github.com/eolinker/apinto-dashboard/modules/module-plugin"
 	"github.com/eolinker/apinto-dashboard/modules/module-plugin/entry"
-	"github.com/go-redis/redis/v8"
 	"time"
 )
 
-func newNavigationModulesCache(client *redis.ClusterClient) module_plugin.INavigationModulesCache {
-	return cache.CreateRedisCacheNoKey[entry.EnabledModule](client, 10*time.Minute, "navigationModulesCacheKey")
+func newNavigationModulesCache() module_plugin.INavigationModulesCache {
+	return cache.CreateRedisCacheNoKey[entry.EnabledModule](10*time.Minute, "navigationModulesCacheKey")
 
 }
