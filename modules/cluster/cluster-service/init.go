4ccf4b4d8a30a9a94afc332ef4603223a8a30a5e
chenjiekun <chenjiekun@sina.cn>
2023-06-27 12:36:17 +0800

    集群节点加缓存

--- modules/cluster/cluster-service/init.go
+++ modules/cluster/cluster-service/init.go
@@ -1,7 +1,9 @@
 package cluster_service
 
 import (
+	"github.com/eolinker/apinto-dashboard/cache"
 	"github.com/eolinker/eosc/common/bean"
+	"github.com/go-redis/redis/v8"
 )
 
 func init() {
@@ -25,4 +27,9 @@ func init() {
 	bean.Injection(&clusterCertificate)
 	bean.Injection(&clusterNode)
 	//bean.Injection(&clusterConfig)
+
+	cache.RegisterCacheInitHandler(func(client *redis.ClusterClient) {
+		nodeCache := newINodeCache(client)
+		bean.Injection(&nodeCache)
+	})
 }
