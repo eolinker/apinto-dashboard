b29133a75c4ee178aff8ac66fe912365d379399c
maggieyyy <61950669+maggieyyy@users.noreply.github.com>
2023-06-28 16:05:56 +0800

    frontend-business:节点预警按钮路由控制

--- frontend/projects/core/src/app/layout/basic-layout/basic-layout.component.ts
+++ frontend/projects/core/src/app/layout/basic-layout/basic-layout.component.ts
@@ -157,13 +157,12 @@ export class BasicLayoutComponent implements OnInit {
     this.subAuthCheck.unsubscribe()
     this.subAuthCheck = this.api.authGet('activation/check').subscribe((resp:{code:number, msg:string, data:{status:'normal'|'waring'|'freeze', prompt:string, label:string}}) => {
       if (resp.code === 0) {
-        if (resp.data.status === 'freeze') {
-          this.router.navigate(['/', 'auth-info'])
-          return
-        }
         this.authStatus = resp.data.status
         this.btnLabel = resp.data.label
         this.btnTooltip = resp.data.prompt
+        if (resp.data.status === 'freeze') {
+          this.router.navigate(['/', 'auth-info'])
+        }
       }
     })
   }
@@ -210,6 +209,10 @@ export class BasicLayoutComponent implements OnInit {
       this.sideMenuOptions = [...this.sideMenuOptions]
       this.currentRouter = router
     }
+  }
+
+  showAuthBtn () {
+    return !this.router.url.includes('auth-info') && this.isBusiness && this.authStatus && this.authStatus !== 'normal'
   }
 
   goToAuth () {
